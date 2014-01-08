0cc9f76d7405e158d2d9713c33388a7786886a85
Brian Drummond <brian@shapes.demon.co.uk>
2014-01-08 20:40:42 +0000

    Update version.ads for 0.32dev (default branch)

--- version.ads
+++ version.ads
@@ -1,5 +1,5 @@
 package Version is
    Ghdl_Release : constant String :=
-      "GHDL 0.31dev (20132311) [Dunoon edition]";
-   Ghdl_Ver : constant String := "0.31dev";
+      "GHDL 0.32dev (20140801) [Dunoon edition]";
+   Ghdl_Ver : constant String := "0.32dev";
 end Version;
