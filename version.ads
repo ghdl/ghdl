215cf4abf6391fe799b83bd68204873d880b212b
Tristan Gingold <tgingold@free.fr>
2014-01-09 06:25:01 +0100

    Fix date (use ISO format YYYYMMDD)

--- version.ads
+++ version.ads
@@ -1,5 +1,5 @@
 package Version is
    Ghdl_Release : constant String :=
-      "GHDL 0.31 (20140801) [Dunoon edition]";
+      "GHDL 0.31 (20140108) [Dunoon edition]";
    Ghdl_Ver : constant String := "0.31";
 end Version;
