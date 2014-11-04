184a123f91e07c927292d67462561dc84f3a920d
Tristan Gingold <tgingold@free.fr>
2014-11-04 20:05:19 +0100

    Bump version.

--- version.ads
+++ version.ads
@@ -1,5 +1,5 @@
 package Version is
    Ghdl_Release : constant String :=
-      "GHDL 0.32dev (20140801) [Dunoon edition]";
-   Ghdl_Ver : constant String := "0.32dev";
+      "GHDL 0.33dev (20141104) [Dunoon edition]";
+   Ghdl_Ver : constant String := "0.33dev";
 end Version;
