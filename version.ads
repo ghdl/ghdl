20d64204f4f44855197c246fec98aefd2078a877
Tristan Gingold <tgingold@free.fr>
2014-11-08 07:15:33 +0100

    Set rc1 version.

--- version.ads
+++ version.ads
@@ -1,5 +1,5 @@
 package Version is
    Ghdl_Release : constant String :=
-      "GHDL 0.32 (20141104) [Dunoon edition]";
-   Ghdl_Ver : constant String := "0.32";
+      "GHDL 0.32rc1 (20141104) [Dunoon edition]";
+   Ghdl_Ver : constant String := "0.32rc1";
 end Version;
