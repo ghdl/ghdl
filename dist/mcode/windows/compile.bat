2ed44b53f4c88ff4253ba1bfdb69ba7405046ef3
Tristan Gingold <tgingold@free.fr>
2015-01-21 04:57:25 +0100

    Fixed paths in winbuild.bat to reflect source tree reorganization (Brian Davis)

--- dist/mcode/windows/compile.bat
+++ dist/mcode/windows/compile.bat
@@ -3,12 +3,12 @@ cd build
 
 rem Do the compilation
 set CFLAGS=-O -g
-gcc -c %CFLAGS% ../../grt/grt-cbinding.c
-gcc -c %CFLAGS% ../../grt/grt-cvpi.c
-gcc -c %CFLAGS% ../../grt/config/clock.c
-gcc -c %CFLAGS% ../../../ortho/mcode/memsegs_c.c
-gcc -c %CFLAGS% -DWITH_GNAT_RUN_TIME ../../grt/config/win32.c
-gnatmake %CFLAGS% -gnatn -aI../windows -aI../../.. -aI../.. -aI../../ghdldrv -aI../../../psl -aI../../grt -aI../../../ortho/mcode ghdl_jit -aI../../../ortho -o ghdl.exe -largs grt-cbinding.o clock.o grt-cvpi.o memsegs_c.o win32.o -largs -Wl,--stack,8404992
+gcc -c %CFLAGS% ../../../src/grt/grt-cbinding.c
+gcc -c %CFLAGS% ../../../src/grt/grt-cvpi.c
+gcc -c %CFLAGS% ../../../src/grt/config/clock.c
+gcc -c %CFLAGS% ../../../src/ortho/mcode/memsegs_c.c
+gcc -c %CFLAGS% -DWITH_GNAT_RUN_TIME ../../../src/grt/config/win32.c
+gnatmake %CFLAGS% -gnatn -aI../windows -aI../../../src -aI../../../src/ghdldrv -aI../../../src/psl -aI../../../src/grt -aI../../../src/ortho/mcode -aI../../../src/vhdl -aI../../../src/vhdl/translate ghdl_jit -aI../../../src/ortho -o ghdl.exe -largs grt-cbinding.o clock.o grt-cvpi.o memsegs_c.o win32.o -largs -Wl,--stack,8404992
 
 if errorlevel 1 goto failed
 
