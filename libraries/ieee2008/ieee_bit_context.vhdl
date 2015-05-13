a85cce9b6f89324dead5da3550af2a71da94d85f
Tristan Gingold <tgingold@free.fr>
2015-05-13 07:54:42 +0200

    Add ieee context files.

---
+++ libraries/ieee2008/ieee_bit_context.vhdl
@@ -0,0 +1,4 @@
+context IEEE_BIT_CONTEXT is
+  library IEEE;
+  use IEEE.NUMERIC_BIT.all;
+end context IEEE_BIT_CONTEXT;
---
+++ libraries/ieee2008/ieee_std_context.vhdl
@@ -0,0 +1,5 @@
+context IEEE_STD_CONTEXT is
+  library IEEE;
+  use IEEE.STD_LOGIC_1164.all;
+  use IEEE.NUMERIC_STD.all;
+end context IEEE_STD_CONTEXT;
