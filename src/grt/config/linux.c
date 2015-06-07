20d187f21a8a7718e4f4942a7fcb072ebcbf4527
Tristan Gingold <tgingold@free.fr>
2015-06-07 07:19:37 +0200

    Initial support of DragonFly.

--- src/grt/config/linux.c
+++ src/grt/config/linux.c
@@ -44,7 +44,7 @@
 #define EXTEND_STACK 1
 #define STACK_SIGNAL SIGSEGV
 #endif
-#ifdef __FreeBSD__
+#if defined (__FreeBSD__) || defined (__DragonFly__)
 /* If set, SIGSEGV is caught in order to automatically grow the stacks.  */
 #define EXTEND_STACK 1
 #define STACK_SIGNAL SIGSEGV
