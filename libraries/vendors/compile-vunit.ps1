16222efe0d37e9e10822f592a11483dd2a4a15fa
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2015-11-24 11:27:42 +0100

    Changed copyright line in file header.

--- libraries/vendors/compile-vunit.ps1
+++ libraries/vendors/compile-vunit.ps1
@@ -14,7 +14,7 @@
 #		- compiles all VUnit packages 
 #
 # ==============================================================================
-#	Copyright (C) 2002, 2003, 2004, 2005 Tristan Gingold
+#	Copyright (C) 2015 Patrick Lehmann
 #	
 #	GHDL is free software; you can redistribute it and/or modify it under
 #	the terms of the GNU General Public License as published by the Free
--- libraries/vendors/compile-xilinx-vivado.ps1
+++ libraries/vendors/compile-xilinx-vivado.ps1
@@ -15,7 +15,7 @@
 #		- compiles all Xilinx Vivado simulation libraries and packages
 #
 # ==============================================================================
-#	Copyright (C) 2002, 2003, 2004, 2005 Tristan Gingold
+#	Copyright (C) 2015 Patrick Lehmann
 #	
 #	GHDL is free software; you can redistribute it and/or modify it under
 #	the terms of the GNU General Public License as published by the Free
