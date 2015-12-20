92ac42fcf39d5dac4529d939a49437b2a7afe4c2
Tristan Gingold <gingold@adacore.com>
2015-12-20 13:56:08 +0100

    openieee: complete math_real (and fix uniform).

--- libraries/openieee/math_real-body.vhdl
+++ libraries/openieee/math_real-body.vhdl
@@ -46,22 +46,60 @@ package body MATH_REAL is
   function TRUNC (X : REAL) return REAL is
   begin
     assert false severity failure;
-  end TRUNC;
+  end;
+
+  function fmod (X, Y : REAL) return REAL;
+  attribute foreign of fmod : function is "VHPIDIRECT fmod";
+
+  function fmod (X, Y : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function "mod" (X, Y : REAL) return REAL
+  is
+    variable res : real;
+  begin
+    assert y /= 0.0 report "ieee.math_real.""mod"": dividend is 0.0"
+      severity failure;
+    res := fmod (x, y);
+    if res /= 0.0 then
+      if x > 0.0 xor y > 0.0 then
+        res := res + y;
+      end if;
+    end if;
+    return res;
+  end "mod";
+
+  function REALMAX (X, Y : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function REALMIN (X, Y : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
 
   procedure UNIFORM (SEED1, SEED2 : inout POSITIVE; X : out REAL)
   is
     variable z, k : Integer;
+    variable s1, s2 : Integer;
   begin
     k := seed1 / 53668;
-    seed1 := 40014 * (seed1 - k * 53668) - k * 12211;
-    if seed1 < 0 then
-      seed1 := seed1 + 2147483563;
+    s1 := 40014 * (seed1 - k * 53668) - k * 12211;
+    if s1 < 0 then
+      seed1 := s1 + 2147483563;
+    else
+      seed1 := s1;
     end if;
 
     k := seed2 / 52774;
-    seed2 := 40692 * (seed2 - k * 52774) - k * 3791;
-    if seed2 < 0 then
-      seed2 := seed2 + 2147483399;
+    s2 := 40692 * (seed2 - k * 52774) - k * 3791;
+    if s2 < 0 then
+      seed2 := s2 + 2147483399;
+    else
+      seed2 := s2;
     end if;
 
     z := seed1 - seed2;
@@ -72,15 +110,114 @@ package body MATH_REAL is
     x := real (z) * 4.656613e-10;
   end UNIFORM;
 
+  function SQRT (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function CBRT (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function "**" (X : INTEGER; Y : REAL) return REAL is
+  begin
+    return real (x) ** y;
+  end "**";
+
+  function "**" (X : REAL; Y : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function EXP (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function LOG (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function LOG2 (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function LOG10 (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function LOG (X : REAL; BASE : REAL) return REAL is
+  begin
+    return log (x) / log (base);
+  end log;
+
   function SIN (X : REAL) return REAL is
   begin
     assert false severity failure;
-  end SIN;
+  end;
 
   function COS (X : REAL) return REAL is
   begin
     assert false severity failure;
-  end COS;
+  end;
 
+  function TAN (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function ARCSIN (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function ARCCOS (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function ARCTAN (Y : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function ARCTAN (Y, X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function SINH (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function COSH (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function TANH (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function ARCSINH (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function ARCCOSH (X : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
+
+  function ARCTANH (Y : REAL) return REAL is
+  begin
+    assert false severity failure;
+  end;
 
 end MATH_REAL;
--- src/ghdldrv/foreigns.adb
+++ src/ghdldrv/foreigns.adb
@@ -30,26 +30,65 @@ package body Foreigns is
    function Trunc (Arg : double) return double;
    pragma Import (C, Trunc);
 
+   function Fmod (X, Y : double) return double;
+   pragma Import (C, Fmod);
+
+   function Fmin (X, Y : double) return double;
+   pragma Import (C, Fmin);
+
+   function Fmax (X, Y : double) return double;
+   pragma Import (C, Fmax);
+
    function Sin (Arg : double) return double;
    pragma Import (C, Sin);
 
    function Cos (Arg : double) return double;
    pragma Import (C, Cos);
 
+   function Tan (Arg : double) return double;
+   pragma Import (C, Tan);
+
+   function Atan (Y : double) return double;
+   pragma Import (C, Atan);
+
+   function Atan2 (X, Y : double) return double;
+   pragma Import (C, Atan2);
+
    function Log (Arg : double) return double;
    pragma Import (C, Log);
+
+   function Log2 (Arg : double) return double;
+   pragma Import (C, Log2);
+
+   function Log10 (Arg : double) return double;
+   pragma Import (C, Log10);
 
    function Exp (Arg : double) return double;
    pragma Import (C, Exp);
 
+   function Pow (X, Y : double) return double;
+   pragma Import (C, Pow);
+
    function Sqrt (Arg : double) return double;
    pragma Import (C, Sqrt);
+
+   function Cbrt (Arg : double) return double;
+   pragma Import (C, Cbrt);
 
    function Asin (Arg : double) return double;
    pragma Import (C, Asin);
 
    function Acos (Arg : double) return double;
    pragma Import (C, Acos);
+
+   function Sinh (Arg : double) return double;
+   pragma Import (C, Sinh);
+
+   function Cosh (Arg : double) return double;
+   pragma Import (C, Cosh);
+
+   function Tanh (Arg : double) return double;
+   pragma Import (C, Tanh);
 
    function Asinh (Arg : double) return double;
    pragma Import (C, Asinh);
@@ -59,9 +98,6 @@ package body Foreigns is
 
    function Atanh (X : double) return double;
    pragma Import (C, Atanh);
-
-   function Atan2 (X, Y : double) return double;
-   pragma Import (C, Atan2);
 
    type String_Cacc is access constant String;
    type Foreign_Record is record
@@ -75,17 +111,29 @@ package body Foreigns is
       (new String'("floor"), Floor'Address),
       (new String'("round"), Round'Address),
       (new String'("trunc"), Trunc'Address),
-      (new String'("sin"), Sin'Address),
-      (new String'("cos"), Cos'Address),
+      (new String'("fmod"), Fmod'Address),
+      (new String'("fmin"), Fmin'Address),
+      (new String'("fmax"), Fmax'Address),
       (new String'("log"), Log'Address),
+      (new String'("log2"), Log2'Address),
+      (new String'("log10"), Log10'Address),
       (new String'("exp"), Exp'Address),
       (new String'("sqrt"), Sqrt'Address),
+      (new String'("cbrt"), Cbrt'Address),
+      (new String'("pow"), Pow'Address),
+      (new String'("sin"), Sin'Address),
+      (new String'("cos"), Cos'Address),
+      (new String'("tan"), Tan'Address),
       (new String'("asin"), Asin'Address),
       (new String'("acos"), Acos'Address),
+      (new String'("atan"), Atan'Address),
+      (new String'("atan2"), Atan2'Address),
+      (new String'("sinh"), Sinh'Address),
+      (new String'("cosh"), Cosh'Address),
+      (new String'("tanh"), Tanh'Address),
       (new String'("asinh"), Asinh'Address),
       (new String'("acosh"), Acosh'Address),
-      (new String'("atanh"), Atanh'Address),
-      (new String'("atan2"), Atan2'Address)
+      (new String'("atanh"), Atanh'Address)
      );
 
    function Find_Foreign (Name : String) return Address is
