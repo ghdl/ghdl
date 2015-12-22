2ac84541d494e06b7782cd11515264fd43f7532f
Tristan Gingold <gingold@adacore.com>
2015-12-22 22:04:16 +0100

    Add parse test for ams.

---
+++ testsuite/gna/bug031/ams1.vhdl
@@ -0,0 +1,13 @@
+entity ams1 is
+end;
+
+architecture behav of ams1 is
+  nature electrical is real across real through ground reference;
+  --  terminal nx : electrical;
+  --  quantity energy : real;
+begin
+  process
+  begin
+    wait;
+  end process;
+end behav;
