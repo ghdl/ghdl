e3c9103b4c6e5bd3d3d8d4a053b7a6c05ff55aeb
Tristan Gingold <gingold@adacore.com>
2016-01-03 21:42:32 +0100

    Add test for html output.

---
+++ testsuite/gna/bug033/testsuite.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+. ../../testenv.sh
+
+analyze simple.vhdl
+elab_simulate simple
+
+$GHDL --reprint simple.vhdl
+$GHDL --pp-html simple.vhdl > /dev/null
+$GHDL --xref-html simple.vhdl
+$GHDL --xref-html --std=08 simple08.vhdl
+
+clean
+
+echo "Test successful"
