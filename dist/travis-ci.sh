dac6746d21480b5f60e0260d280cc22a6c7d72ea
Tristan Gingold <tgingold@free.fr>
2016-01-14 21:09:48 +0100

    travis: use envs.

--- dist/travis-ci.sh
+++ dist/travis-ci.sh
@@ -6,33 +6,35 @@ set -e
 
 CDIR=$PWD
 
-# Build mcode64
-mkdir build-mcode64
-mkdir install-mcode64
-cd build-mcode64
-../configure --prefix=$CDIR/install-mcode64
+# Prepare
+mkdir build-$1
+mkdir install-$1
+cd build-$1
+
+# Configure
+case "$1" in
+  mcode)
+    ../configure --prefix=$CDIR/install-mcode64
+    ;;
+
+  llvm)
+    ../configure --prefix=$CDIR/install-llvm --with-llvm-config=llvm-config-3.5
+    ;;
+
+  *)
+    echo "unknown build $1"
+    exit 1
+    ;;
+esac
+
+# Build
 make
 make install
 cd ..
 
-# Test mcode64
-export GHDL=$CDIR/install-mcode64/bin/ghdl
+# Test
+export GHDL=$CDIR/install-$1/bin/ghdl
 cd testsuite
 gnatmake get_entities
-./testsuite.sh
-cd ..
-
-# build for llvm
-mkdir build-llvm
-mkdir install-llvm
-cd build-llvm
-../configure --prefix=$CDIR/install-llvm --with-llvm-config=llvm-config-3.5
-make
-make install
-cd ..
-
-# Test llvm
-export GHDL=$CDIR/install-llvm/bin/ghdl
-cd testsuite
 ./testsuite.sh
 cd ..
