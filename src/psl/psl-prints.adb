506d49798a78f67735c9a529eb849c152ced1e4b
Tristan Gingold <tgingold@free.fr>
2016-01-27 05:10:10 +0100

    psl: use Tables instead of GNAT.Table, regenerate, add copyrights.

--- src/psl/psl-prints.adb
+++ src/psl/psl-prints.adb
@@ -1,3 +1,21 @@
+--  PSL - Pretty print
+--  Copyright (C) 2002-2016 Tristan Gingold
+--
+--  GHDL is free software; you can redistribute it and/or modify it under
+--  the terms of the GNU General Public License as published by the Free
+--  Software Foundation; either version 2, or (at your option) any later
+--  version.
+--
+--  GHDL is distributed in the hope that it will be useful, but WITHOUT ANY
+--  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+--  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+--  for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with GHDL; see the file COPYING.  If not, write to the Free
+--  Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+--  02111-1307, USA.
+
 with Types; use Types;
 with PSL.Errors; use PSL.Errors;
 with Name_Table; use Name_Table;
@@ -430,4 +448,3 @@ package body PSL.Prints is
       Put_Line ("}");
    end Print_Unit;
 end PSL.Prints;
-
