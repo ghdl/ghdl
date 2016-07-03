53b1d9101cbff266c4c34c72dd77970bba0e9718
Tristan Gingold <tgingold@free.fr>
2016-07-03 16:31:22 +0200

    bug011: also tests vcd.

--- testsuite/gna/bug011/testsuite.sh
+++ testsuite/gna/bug011/testsuite.sh
@@ -7,8 +7,11 @@ elab phonybench
 if ghdl_has_feature phonybench fst; then
   elab_simulate phonybench --stop-time=1sec --fst=pb.fst
 fi
+if ghdl_has_feature phonybench vcd; then
+  elab_simulate phonybench --stop-time=1sec --vcd=pb.vcd
+fi
 
-rm -f pb.fst pb.ghw
+rm -f pb.fst pb.vcd
 
 clean
 
