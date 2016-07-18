27ec9a1711320105bce54b27589b4d5922f67f8b
Tristan Gingold <tgingold@free.fr>
2016-07-18 07:01:33 +0200

    Add testcase for previous patch (synopsys package not found).

---
+++ testsuite/gna/bug047/repro_arith.vhdl
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_arith.all;
+
+entity repro_arith is
+end repro_arith;
+
+architecture behav of repro_arith is
+  signal s : unsigned (7 downto 0) := x"00";
+begin
+  process
+  begin
+    s <= s + 1;
+    wait for 1 ns;
+  end process;
+end behav;
