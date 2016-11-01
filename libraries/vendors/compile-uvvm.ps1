3a98fcd5d140435e063d0979a3494dbf4af4198f
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-11-01 23:56:26 +0100

    Implemented a compile-script for UVVM.

---
+++ libraries/vendors/compile-uvvm.ps1
@@ -0,0 +1,158 @@
+# EMACS settings: -*-	tab-width: 2; indent-tabs-mode: t -*-
+# vim: tabstop=2:shiftwidth=2:noexpandtab
+# kate: tab-width 2; replace-tabs off; indent-width 2;
+# 
+# ==============================================================================
+#	Authors:						Patrick Lehmann
+# 
+#	PowerShell Script:	Script to compile the UVVM library for GHDL on Windows
+# 
+# Description:
+# ------------------------------------
+#	This is a PowerShell script (executable) which:
+#		- creates a subdirectory in the current working directory
+#		- compiles all UVVM packages 
+#
+# ==============================================================================
+#	Copyright (C) 2015-2016 Patrick Lehmann - Dresden, Germany
+#	
+#	GHDL is free software; you can redistribute it and/or modify it under
+#	the terms of the GNU General Public License as published by the Free
+#	Software Foundation; either version 2, or (at your option) any later
+#	version.
+#	
+#	GHDL is distributed in the hope that it will be useful, but WITHOUT ANY
+#	WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#	FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#	for more details.
+#	
+#	You should have received a copy of the GNU General Public License
+#	along with GHDL; see the file COPYING.  If not, write to the Free
+#	Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+#	02111-1307, USA.
+# ==============================================================================
+
+# .SYNOPSIS
+# This CmdLet compiles the UVVM library.
+# 
+# .DESCRIPTION
+# This CmdLet:
+#   (1) creates a subdirectory in the current working directory
+#   (2) compiles all UVVM packages
+#
+[CmdletBinding()]
+param(
+	# Show the embedded help page(s)
+	[switch]$Help =							$false,
+	
+	# Compile all packages.
+	[switch]$All =							$true,
+	
+	# Compile all UVVM packages.
+	[switch]$UVVM =							$true,
+	
+	# Clean up directory before analyzing.
+	[switch]$Clean =						$false,
+	
+	#Skip warning messages. (Show errors only.)
+	[switch]$SuppressWarnings = $false,
+	# Halt on errors
+	[switch]$HaltOnError =			$false,
+	
+	# Set vendor library source directory
+	[string]$Source =						"",
+	# Set output directory name
+	[string]$Output =						"",
+	# Set GHDL executable
+	[string]$GHDL =							""
+)
+
+# ---------------------------------------------
+# save working directory
+$WorkingDir =		Get-Location
+
+# set default values
+$EnableVerbose =			$PSCmdlet.MyInvocation.BoundParameters["Verbose"]
+$EnableDebug =				$PSCmdlet.MyInvocation.BoundParameters["Debug"]
+if ($EnableVerbose -eq $null)	{	$EnableVerbose =	$false	}
+if ($EnableDebug	 -eq $null)	{	$EnableDebug =		$false	}
+if ($EnableDebug	 -eq $true)	{	$EnableVerbose =	$true		}
+
+# load modules from GHDL's 'vendors' library directory
+Import-Module $PSScriptRoot\config.psm1 -Verbose:$false -Debug:$false -ArgumentList "UVVM"
+Import-Module $PSScriptRoot\shared.psm1 -Verbose:$false -Debug:$false -ArgumentList @("UVVM", "$WorkingDir")
+
+# Display help if no command was selected
+$Help = $Help -or (-not ($All -or $UVVM -or $Clean))
+
+if ($Help)
+{	Get-Help $MYINVOCATION.InvocationName -Detailed
+	Exit-CompileScript
+}
+if ($All)
+{	$UVVM =			$true
+}
+
+
+$SourceDirectory =			Get-SourceDirectory $Source ""
+$DestinationDirectory =	Get-DestinationDirectory $Output
+$GHDLBinary =						Get-GHDLBinary $GHDL
+
+# create "Altera" directory and change to it
+New-DestinationDirectory $DestinationDirectory
+cd $DestinationDirectory
+
+
+$VHDLVersion,$VHDLStandard,$VHDLFlavor = Get-VHDLVariables
+
+# define global GHDL Options
+$GHDLOptions = @("-a", "-fexplicit", "-frelaxed-rules", "--mb-comments", "--warn-binding", "--ieee=$VHDLFlavor", "--no-vital-checks", "--std=$VHDLStandard", "-P$DestinationDirectory")
+
+# extract data from configuration
+# $SourceDir =			$InstallationDirectory["AlteraQuartus"] + "\quartus\eda\sim_lib"
+
+$ErrorCount =			0
+
+# Cleanup directories
+# ==============================================================================
+if ($Clean)
+{	Write-Host "[ERROR]: '-Clean' is not implemented!" -ForegroundColor Red
+	Exit-CompileScript -1
+	
+	Write-Host "Cleaning up vendor directory ..." -ForegroundColor Yellow
+	rm *.cf
+}
+
+
+# UVVM packages
+# ==============================================================================
+# compile uvvm_util library
+if ((-not $StopCompiling) -and $UVVM)
+{	$Library = "uvvm_util"
+	$Files = @(
+		"src\types_pkg.vhd",
+		"src\adaptations_pkg.vhd",
+		"src\string_methods_pkg.vhd",
+		"src\protected_types_pkg.vhd",
+		"src\hierarchy_linked_list_pkg.vhd",
+		"src\alert_hierarchy_pkg.vhd",
+		"src\license_pkg.vhd",
+		"src\methods_pkg.vhd",
+		"src\bfm_common_pkg.vhd",
+		"src\uvvm_util_context.vhd"
+	)
+	$SourceFiles = $Files | % { "$SourceDirectory\$_" }
+	
+	$ErrorCount += 0
+	Start-PackageCompilation $GHDLBinary $GHDLOptions $DestinationDirectory $Library $VHDLVersion $SourceFiles $HaltOnError -Verbose:$EnableVerbose -Debug:$EnableDebug
+	$StopCompiling = $HaltOnError -and ($ErrorCount -ne 0)
+}
+
+Write-Host "--------------------------------------------------------------------------------"
+Write-Host "Compiling UVVM packages " -NoNewline
+if ($ErrorCount -gt 0)
+{	Write-Host "[FAILED]" -ForegroundColor Red				}
+else
+{	Write-Host "[SUCCESSFUL]" -ForegroundColor Green	}
+
+Exit-CompileScript
