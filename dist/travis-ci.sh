5ccabe2c5f9d88b8f3065cafc8c156b35f5fb502
Tristan Gingold <tgingold@free.fr>
2016-12-12 03:55:23 +0100

    travis: set CLANGXX in make command

--- dist/travis-ci.sh
+++ dist/travis-ci.sh
@@ -20,21 +20,24 @@ cd build-$BLD
 # Configure
 case "$BLD" in
   mcode)
-    ../configure --prefix="$prefix" ;;
+      ../configure --prefix="$prefix"
+      MAKEOPTS=""
+      ;;
 
-  llvm-*)
-    llvm_ver=`echo $BLD | sed -e 's/llvm-//'`
-    ../configure --prefix="$prefix" --with-llvm-config=llvm-config-$llvm_ver
-    ;;
+  llvm*)
+      llvm_ver=`echo $BLD | sed -e 's/llvm//'`
+      ../configure --prefix="$prefix" --with-llvm-config=llvm-config$llvm_ver
+      MAKEOPTS="CLANGXX=clang++$llvm_ver"
+      ;;
 
   *)
-    echo "unknown build $BLD"
-    exit 1
-    ;;
+      echo "unknown build $BLD"
+      exit 1
+      ;;
 esac
 
 # Build
-make
+make $MAKEOPTS
 make install
 cd ..
 
