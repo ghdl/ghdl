d0dd4dacc27724072f1a43dea2dc4b6878c10ccb
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-16 19:55:25 +0100

    Debugging gcc ...

---
+++ dist/appveyor.debug.ps1
@@ -0,0 +1,11 @@
+c:\msys64\mingw64\bin\gcc.exe -print-search-dirs
+
+c:\msys64\mingw64\bin\cpp.exe -print-search-dirs
+
+c:\msys64\mingw64\bin\g++.exe -print-search-dirs
+
+c:\msys64\mingw64\bin\gcc.exe -v
+
+c:\msys64\mingw64\bin\cpp.exe -v
+
+c:\msys64\mingw64\bin\g++.exe -v
