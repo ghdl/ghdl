5fe9993a1e8dc8738f8dde6163daf86637a18f3b
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-17 08:42:07 +0100

    Debugging clang.exe

--- dist/appveyor.debug.ps1
+++ dist/appveyor.debug.ps1
@@ -1,11 +1,11 @@
+Write-Host "--------------------"
 c:\msys64\mingw64\bin\gcc.exe -print-search-dirs
-
-c:\msys64\mingw64\bin\cpp.exe -print-search-dirs
-
-c:\msys64\mingw64\bin\g++.exe -print-search-dirs
-
+Write-Host "--------------------"
+c:\msys64\mingw64\bin\clang.exe -print-search-dirs
+Write-Host "--------------------"
+c:\msys64\mingw64\bin\clang++.exe -print-search-dirs
+Write-Host "--------------------"
 c:\msys64\mingw64\bin\gcc.exe -v
 
-c:\msys64\mingw64\bin\cpp.exe -v
-
-c:\msys64\mingw64\bin\g++.exe -v
+# c:\msys64\mingw64\bin\cpp.exe -v
+# c:\msys64\mingw64\bin\g++.exe -v
