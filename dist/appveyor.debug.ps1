73101a704b8751c8adfc2ce46a859df45c8a80af
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-17 09:10:45 +0100

    Convert STDERR objects.

--- dist/appveyor.debug.ps1
+++ dist/appveyor.debug.ps1
@@ -3,6 +3,6 @@ c:\msys64\mingw64\bin\gcc.exe -print-search-dirs
 Write-Host "--------------------"
 c:\msys64\mingw64\bin\clang.exe -print-search-dirs
 Write-Host "--------------------"
-#c:\msys64\mingw64\bin\gcc.exe -v
+c:\msys64\mingw64\bin\gcc.exe -v 2>&1 | %{ "$_" }
 # c:\msys64\mingw64\bin\cpp.exe -v
 # c:\msys64\mingw64\bin\g++.exe -v
