2eb46f5a629d1048cace670b289b8a8d8261c043
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-17 09:41:19 +0100

    YAML file cleanup

--- dist/appveyor.debug.ps1
+++ dist/appveyor.debug.ps1
@@ -1,8 +1,10 @@
-Write-Host "--------------------"
-c:\msys64\mingw64\bin\gcc.exe -print-search-dirs
-Write-Host "--------------------"
-c:\msys64\mingw64\bin\clang.exe -print-search-dirs
-Write-Host "--------------------"
+Write-Host "Print env:PATH..."
+$env:PATH.Split(";") | % { Write-Host "  $_" }
+Write-Host "Print GCC setup..."
 c:\msys64\mingw64\bin\gcc.exe -v 2>&1 | %{ "$_" }
-# c:\msys64\mingw64\bin\cpp.exe -v
-# c:\msys64\mingw64\bin\g++.exe -v
+Write-Host "Print GCC search directories..."
+c:\msys64\mingw64\bin\gcc.exe -print-search-dirs
+Write-Host "Print CLang setup..."
+c:\msys64\mingw64\bin\clang.exe -v 2>&1 | %{ "$_" }
+Write-Host "Print CLang search directories..."
+c:\msys64\mingw64\bin\clang.exe -print-search-dirs
