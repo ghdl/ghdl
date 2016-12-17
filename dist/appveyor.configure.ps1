871330dc16abe41feb1f43e01ee323bd0091a189
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-17 11:55:32 +0100

    Fixed --prefix. It must be a MinGW compatible path.

--- dist/appveyor.configure.ps1
+++ dist/appveyor.configure.ps1
@@ -49,7 +49,7 @@ function Restore-NativeCommandStream
 Write-Host "Configuring GHDL for MinGW64, LLVM-3.5..."
 
 $GHDL_BUILD_DIR =  "$($env:APPVEYOR_BUILD_FOLDER)\build\mingw64-llvm"
-$GHDL_PREFIX_DIR = "C:\Tools\GHDL-0.34-dev-mingw64-llvm"
+$GHDL_PREFIX_DIR = "/c/Tools/GHDL/0.34-dev-mingw64-llvm"
 
 $env:GHDL_BUILD_DIR =  $GHDL_BUILD_DIR
 $env:GHDL_PREFIX_DIR = $GHDL_PREFIX_DIR
