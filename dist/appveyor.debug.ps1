5a6376cdcf699346fbecb603c7346f0ee98cf1f9
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-17 09:04:25 +0100

    Call make.

--- dist/appveyor.debug.ps1
+++ dist/appveyor.debug.ps1
@@ -3,9 +3,6 @@ c:\msys64\mingw64\bin\gcc.exe -print-search-dirs
 Write-Host "--------------------"
 c:\msys64\mingw64\bin\clang.exe -print-search-dirs
 Write-Host "--------------------"
-c:\msys64\mingw64\bin\clang++.exe -print-search-dirs
-Write-Host "--------------------"
-c:\msys64\mingw64\bin\gcc.exe -v
-
+#c:\msys64\mingw64\bin\gcc.exe -v
 # c:\msys64\mingw64\bin\cpp.exe -v
 # c:\msys64\mingw64\bin\g++.exe -v
