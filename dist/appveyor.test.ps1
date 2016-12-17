8d5bbe4e83e08e6c9d4e52918981b42bf2a45d92
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-17 15:19:49 +0100

    Fixed $env:GHDL. It must be a MinGW compatible path too.

--- dist/appveyor.test.ps1
+++ dist/appveyor.test.ps1
@@ -49,7 +49,8 @@ function Restore-NativeCommandStream
 Write-Host "Building GHDL and libraries..."
 cd "$($env:APPVEYOR_BUILD_FOLDER)\testsuite"
 
-$env:GHDL="$($env:GHDL_PREFIX_DIR)\bin\ghdl.exe"
+# Use a MinGW compatible path
+$env:GHDL="$($env:GHDL_PREFIX_DIR)/bin/ghdl.exe"
 
 c:\msys64\usr\bin\bash.exe -c "./testsuite.sh" 2>&1 | Restore-NativeCommandStream | %{ "$_" }
 
