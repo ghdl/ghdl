81374cc8f42dad7079e5beef4b59734f2f0a9035
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-18 23:59:35 +0100

    Also installing a new PSGet

--- dist/appveyor/install.ps1
+++ dist/appveyor/install.ps1
@@ -5,13 +5,15 @@ C:\msys64\usr\bin\pacman -V
 # C:\msys64\usr\bin\pacman -Q
 C:\msys64\usr\bin\pacman -S mingw-w64-x86_64-llvm35 mingw-w64-x86_64-clang35 --noconfirm
 
-Write-Host "  Installing Chocolatey packages ..." -Foreground Yellow
+Write-Host "  Installing PSGet ..." -Foreground Yellow
+Invoke-WebRequest "http://psget.net/GetPsGet.ps1" -UseBasicParsing | Invoke-Expression
+Write-Host "  Installing Chocolatey ..." -Foreground Yellow
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
-Write-Host "  Installing PowerShell packages ..." -Foreground Yellow
+Write-Host "  Installing PowerShell modules ..." -Foreground Yellow
 Install-Module -Name Pscx
 
 Write-Host "  Check all Write-* CmdLets ..." -Foreground Yellow
-Get-Command -Verb Write
+Get-Command -Verb Write | Format-Table
 
 exit $LastExitCode
