42d4eec17d788b8712dcdbd56d9a5e28447fbb47
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-19 00:09:09 +0100

    Also installing a new PSGet - 2.

--- dist/appveyor/install.ps1
+++ dist/appveyor/install.ps1
@@ -6,11 +6,12 @@ C:\msys64\usr\bin\pacman -V
 C:\msys64\usr\bin\pacman -S mingw-w64-x86_64-llvm35 mingw-w64-x86_64-clang35 --noconfirm
 
 Write-Host "  Installing PSGet ..." -Foreground Yellow
-Invoke-WebRequest "http://psget.net/GetPsGet.ps1" -UseBasicParsing | Invoke-Expression
-Write-Host "  Installing Chocolatey ..." -Foreground Yellow
-Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+Invoke-WebRequest "http://psget.net/GetPsGet.ps1" | Invoke-Expression
+#Write-Host "  Installing Chocolatey ..." -Foreground Yellow
+#Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
 Write-Host "  Installing PowerShell modules ..." -Foreground Yellow
+Find-Module -Name Pscx
 Install-Module -Name Pscx
 
 Write-Host "  Check all Write-* CmdLets ..." -Foreground Yellow
