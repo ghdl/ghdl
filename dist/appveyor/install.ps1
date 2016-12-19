e4f4f4e720b3ca91f4cc1180fae5bf6b4465414e
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-19 03:10:20 +0100

    Trying code provided by Keith Hill.

--- dist/appveyor/install.ps1
+++ dist/appveyor/install.ps1
@@ -5,17 +5,11 @@ C:\msys64\usr\bin\pacman -V
 # C:\msys64\usr\bin\pacman -Q
 C:\msys64\usr\bin\pacman -S mingw-w64-x86_64-llvm35 mingw-w64-x86_64-clang35 --noconfirm
 
-#Write-Host "  Installing PSGet ..." -Foreground Yellow
-#Invoke-WebRequest "http://psget.net/GetPsGet.ps1" | Invoke-Expression
-#Write-Host "  Installing Chocolatey ..." -Foreground Yellow
-#Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
-#Write-Host "  Installing NuGet ..." -Foreground Yellow
-#(new-object net.webclient).DownloadFile("https://dist.nuget.org/win-x86-commandline/v3.5.0/NuGet.exe", "$($env:APPVEYOR_BUILD_FOLDER)\NuGet.exe")
-#dir
-.\Nuget.exe install pscx
+Write-Host "  Installing NuGet as PackageProvider ..." -Foreground Yellow
+Install-PackageProvider nuget -force
+Import-PackageProvider nuget -force
 
 Write-Host "  Installing PowerShell modules ..." -Foreground Yellow
-Find-Module -Name Pscx
 Install-Module -Name Pscx
 
 Write-Host "  Check all Write-* CmdLets ..." -Foreground Yellow
