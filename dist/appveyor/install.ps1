5998aa620ff09ccd4bd07a0b888fd8e562fe8956
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-19 03:48:24 +0100

    Trust PSGallery.

--- dist/appveyor/install.ps1
+++ dist/appveyor/install.ps1
@@ -1,19 +1,20 @@
 Write-Host "Installing dependencies ..." -Foreground Yellow
-Write-Host "  Installing MinGW64 packages ..." -Foreground Yellow
+Write-Host "----------------------------------------" -Foreground Yellow
+Write-Host "Installing MinGW64 packages ..." -Foreground Yellow
 C:\msys64\usr\bin\pacman -V
 # list installed packages and versions
 # C:\msys64\usr\bin\pacman -Q
 C:\msys64\usr\bin\pacman -S mingw-w64-x86_64-llvm35 mingw-w64-x86_64-clang35 --noconfirm
 
-Write-Host "  Installing NuGet as PackageProvider ..." -Foreground Yellow
-Install-PackageProvider nuget -force
-Import-PackageProvider nuget -force
+Write-Host "Installing NuGet as PackageProvider ..." -Foreground Yellow
+Install-PackageProvider NuGet -Force
+Import-PackageProvider NuGet -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-Write-Host "  Installing PowerShell modules ..." -Foreground Yellow
-Install-Module Pscx -Force -AllowClobber
-#Install-Module Pscx -Scope CurrentUser
+Write-Host "Installing PowerShell modules ..." -Foreground Yellow
+Install-Module Pscx -AllowClobber
 
-Write-Host "  Check all Write-* CmdLets ..." -Foreground Yellow
-Get-Command -Verb Write | Format-Table
+#Write-Host "Check all Write-* CmdLets ..." -Foreground Yellow
+#Get-Command -Verb Write | Format-Table
 
 exit $LastExitCode
