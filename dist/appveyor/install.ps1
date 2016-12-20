a7c8e153534cc5b939b519bf4ecd8d3664d799d3
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-20 01:38:37 +0100

    Install llvm-3.5 for MinGW32.

--- dist/appveyor/install.ps1
+++ dist/appveyor/install.ps1
@@ -1,10 +1,29 @@
 Write-Host "Installing dependencies ..." -Foreground Yellow
 Write-Host "----------------------------------------" -Foreground Yellow
 Write-Host "Installing MinGW64 packages ..." -Foreground Yellow
+
 C:\msys64\usr\bin\pacman -V
 # list installed packages and versions
 # C:\msys64\usr\bin\pacman -Q
-C:\msys64\usr\bin\pacman -S mingw-w64-x86_64-llvm35 mingw-w64-x86_64-clang35 --noconfirm
+
+if ($env:BUILD_MINGW -eq "mingw32")
+{	Write-Host "Installing MinGW32 packages ..." -Foreground Yellow
+	if ($env:BUILD_BACKEND -eq "mcode")
+	{
+	}
+	elseif ($env:BUILD_BACKEND -eq "llvm")
+	{	C:\msys64\usr\bin\pacman -S mingw-w64-i686-llvm35 mingw-w64-i686-clang35 --noconfirm
+	}
+}
+elseif ($env:BUILD_MINGW -eq "mingw64")
+{	Write-Host "Installing MinGW64 packages ..." -Foreground Yellow
+	if ($env:BUILD_BACKEND -eq "mcode")
+	{
+	}
+	elseif ($env:BUILD_BACKEND -eq "llvm")
+	{	C:\msys64\usr\bin\pacman -S mingw-w64-x86_64-llvm35 mingw-w64-x86_64-clang35 --noconfirm
+	}
+}
 
 Write-Host "Installing NuGet as PackageProvider ..." -Foreground Yellow
 Install-PackageProvider NuGet -Force
