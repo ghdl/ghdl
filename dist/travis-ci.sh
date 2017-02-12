87a4f74ad120774197d3e258a725d1b5a0d25a5b
1138-4EB <1138-4EB@users.noreply.github.com>
2017-02-12 08:20:40 +0100

    Extract tag declarations from <travis-ci.sh>. Rename it to <buildtest… (#277)
    * Extract tag declarations from <travis-ci.sh>. Rename it to
    <buildtest.sh> and allow to pass -b (BLD) and -f (PKG_FILE) as
    arguments. Save tag declarations in <travis-ci.sh>, with a different
    meaning now. Modify <.travis.yml> accordingly.
    
    * chmod +x ./dist/buildtest.sh. Can be removed bi changing permissions.

--- dist/travis-ci.sh
+++ dist/travis-ci.sh
@@ -1,52 +1,3 @@
-#! /bin/sh
-# This script is executed in the travis-ci environment.
-
-# Stop in case of error
-set -e
-
-CDIR=$(pwd)
-
-if [ $# -ne 0 ]; then BLD="$1"; fi
-
-# Display environment
-echo "Environment:"
-env
-
-# Prepare
-prefix="$CDIR/install-$BLD"
-mkdir "$prefix"
-mkdir "build-$BLD"
-cd "build-$BLD"
-
-# Configure
-case "$BLD" in
-  mcode)
-      ../configure --prefix="$prefix"
-      MAKEOPTS=""
-      ;;
-
-  llvm-3.5)
-      ../configure --prefix="$prefix" --with-llvm-config=llvm-config-3.5
-      MAKEOPTS="CXX=clang++"
-      ;;
-
-  llvm-3.8)
-      ../configure --prefix="$prefix" --with-llvm-config=llvm-config-3.8
-      MAKEOPTS="CXX=clang++-3.8"
-      ;;
-
-  *)
-      echo "unknown build $BLD"
-      exit 1
-      ;;
-esac
-
-# Build
-make $MAKEOPTS
-make install
-cd ..
-
-# Package
 PKG_VER=`grep Ghdl_Ver src/version.in | sed -e 's/.*"\(.*\)";/\1/'`
 
 if [ "$TRAVIS_TAG" = "" ]; then
@@ -54,13 +5,5 @@ if [ "$TRAVIS_TAG" = "" ]; then
 else
     PKG_TAG="$TRAVIS_TAG"
 fi
-PKG_FILE="ghdl-$PKG_VER-$BLD-$PKG_TAG.tgz"
-echo "creating $PKG_FILE"
-tar -zcvf "$PKG_FILE" -C "$prefix" .
 
-# Test
-export GHDL="$CDIR/install-$BLD/bin/ghdl"
-cd testsuite
-gnatmake get_entities
-./testsuite.sh
-cd ..
+export PKG_FILE="ghdl-$PKG_VER-$BLD-$PKG_TAG.tgz"
