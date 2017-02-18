3a83730e09ca406e21a6bd4637ccddbc10116438
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2017-02-18 07:44:33 +0100

    tar is installed, but wget not

--- dist/windows/appveyor/install.ps1
+++ dist/windows/appveyor/install.ps1
@@ -9,7 +9,7 @@ cd $env:APPVEYOR_BUILD_FOLDER
 
 if ($env:BUILD_BACKEND -eq "gcc")
 {	Write-Host "Installing common MinGW packages ..." -Foreground Yellow
-	C:\msys64\usr\bin\pacman.exe -S tar --noconfirm
+	C:\msys64\usr\bin\pacman.exe -S wget --noconfirm
 }
 
 if ($env:BUILD_MINGW -eq "mingw32")
@@ -33,15 +33,15 @@ elseif ($env:BUILD_MINGW -eq "mingw64")
 
 if ($env:BUILD_BACKEND -eq "gcc")
 {	# Assembles paths
-	$GCC_SOURCE_DIR =     "$($env:APPVEYOR_BUILD_FOLDER)\gcc-$BUILD_GCC_VERSION"
+	$GCC_SOURCE_DIR =     "$($env:APPVEYOR_BUILD_FOLDER)\gcc-$($env:BUILD_GCC_VERSION)"
 	# Export GCC paths as environment variable
 	$env:GCC_SOURCE_DIR = $GCC_SOURCE_DIR
 
 	cd $env:APPVEYOR_BUILD_FOLDER
-	Write-Host "Downloading GCC $BUILD_GCC_VERSION sources ..." -Foreground Yellow
-	wget https://ftp.gnu.org/gnu/gcc/gcc-$BUILD_GCC_VERSION/gcc-$BUILD_GCC_VERSION.tar.gz
+	Write-Host "Downloading GCC $($env:BUILD_GCC_VERSION) sources ..." -Foreground Yellow
+	wget "https://ftp.gnu.org/gnu/gcc/gcc-$($env:BUILD_GCC_VERSION)/gcc-$($env:BUILD_GCC_VERSION).tar.gz"
 	Write-Host "Extracting GCC sources ..." -Foreground Yellow
-	tar xzf gcc-$BUILD_GCC_VERSION.tar.gz
+	tar xzf "gcc-$($env:BUILD_GCC_VERSION).tar.gz"
 }
 
 Write-Host "Installing NuGet as PackageProvider ..." -Foreground Yellow
