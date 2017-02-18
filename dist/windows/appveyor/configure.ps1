435a77c4123edf6611ead1800f214a98ca5aeb85
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2017-02-18 06:51:28 +0100

    Implemented an AppVeyor flow to build GHDL with GCC backend on Windows with MinGW64.

--- dist/windows/appveyor/configure.ps1
+++ dist/windows/appveyor/configure.ps1
@@ -46,12 +46,16 @@ function Restore-NativeCommandStream
 	}
 }
 
+cd $env:APPVEYOR_BUILD_FOLDER
+
+# Assemble paths
 $GHDL_BUILD_DIR =  "$($env:APPVEYOR_BUILD_FOLDER)\build\$($env:BUILD_MINGW)-$($env:BUILD_BACKEND)"
 $GHDL_PREFIX_DIR = "/c/Tools/GHDL/0.34-dev-$($env:BUILD_MINGW)-$($env:BUILD_BACKEND)"
-
+$GCC_BUILD_DIR =   "$($env:APPVEYOR_BUILD_FOLDER)\gcc-$($env:BUILD_GCC_VERSION)\build-$($env:BUILD_MINGW)"
+# Export paths as environment variable
 $env:GHDL_BUILD_DIR =  $GHDL_BUILD_DIR
 $env:GHDL_PREFIX_DIR = $GHDL_PREFIX_DIR
-
+# Create GHDL build directory
 mkdir $GHDL_BUILD_DIR | cd
 
 if ($env:BUILD_BACKEND -eq "mcode")
@@ -61,9 +65,20 @@ if ($env:BUILD_BACKEND -eq "mcode")
 }
 elseif ($env:BUILD_BACKEND -eq "llvm")
 {	Write-Host "Configuring GHDL for $($env:BUILD_MINGW), LLVM-3.5..." -Foreground Yellow
-
 	c:\msys64\usr\bin\bash.exe -c "../../configure --prefix=$GHDL_PREFIX_DIR --with-llvm-config LDFLAGS=-static CXX=g++" 2>&1 | Restore-NativeCommandStream | %{ "$_" }
+}
+elseif ($env:BUILD_BACKEND -eq "gcc")
+{	# Export GCC paths as environment variable
+	$env:GCC_BUILD_DIR =   $GCC_BUILD_DIR
+	# Create GCC build directory
+	mkdir $GCC_BUILD_DIR
+	
+	Write-Host "Configuring GHDL for $($env:BUILD_MINGW), GCC $($env:BUILD_GCC_VERSION)..." -Foreground Yellow
+	c:\msys64\usr\bin\bash.exe -c "../../configure --prefix=$GHDL_PREFIX_DIR --with-gcc=$($env:GCC_SOURCE_DIR) LDFLAGS=-static CXX=g++" 2>&1 | Restore-NativeCommandStream | %{ "$_" }
+	
+	Write-Host "Copy GHDL sources into GCC as a new language (vhdl)..." -Foreground Yellow
+	c:\msys64\usr\bin\make.exe copy-sources 2>&1 | Restore-NativeCommandStream | %{ "$_" }
 }
 
 cd $env:APPVEYOR_BUILD_FOLDER
-exit 0
+exit $LastExitCode
