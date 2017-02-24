674ed3b773ad9249983cb47f986dcf1537420e68
Tristan Gingold <tgingold@free.fr>
2017-02-24 18:49:49 +0100

    Add a test with denormals for #293

--- testsuite/gna/issue295/repro.vhdl
+++ testsuite/gna/issue295/repro.vhdl
@@ -9,6 +9,8 @@ begin
       assert 2.0**(-1024) > 0.0 report "This fails" severity note;
       assert 2.0**(-1024) = 0.0 report "This passes" severity failure;
 
+      assert (2.0**(-1023))/2 > 0.0 report "This fails" severity error;
+
       wait;
     end process test_runner;
 end;
