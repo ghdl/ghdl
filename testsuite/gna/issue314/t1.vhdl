c2c6d63247da8973f8cfcdb04038793bc02eb53e
Tristan Gingold <tgingold@free.fr>
2017-03-14 08:03:28 +0100

    Add testcase for #314

---
+++ testsuite/gna/issue314/t1.vhdl
@@ -0,0 +1,3 @@
+entity t1 is
+	err;
+end t1;
---
+++ testsuite/gna/issue314/testsuite.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+. ../../testenv.sh
+
+analyze_failure t1.vhdl 2> t1.err
+grep 2:8 t1.err
+
+analyze_failure -ftabstop=4 t1.vhdl 2> t1.err
+grep 2:4 t1.err
+
+if analyze -ftabstop=0 t1.vhdl; then
+    echo "error expected"
+    exit 1
+fi
+
+if analyze -ftabstop=140 t1.vhdl; then
+    echo "error expected"
+    exit 1
+fi
+
+if analyze -ftabstop=aa t1.vhdl; then
+    echo "error expected"
+    exit 1
+fi
+
+rm -f t1.err
+
+clean
+
+echo "Test successful"
