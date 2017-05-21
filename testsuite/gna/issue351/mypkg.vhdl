38cfb62cc13100d4d159e83ab7bf8901e1fbf40f
Tristan Gingold <tgingold@free.fr>
2017-05-21 18:52:39 +0200

    Add testcase for #351

---
+++ testsuite/gna/issue351/mypkg.vhdl
@@ -0,0 +1,8 @@
+library ieee;
+use ieee.numeric_std.all;
+
+package MyPkg is
+
+  alias BCDType is unsigned(3 downto 0);
+
+end package MyPkg;
---
+++ testsuite/gna/issue351/testsuite.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+. ../../testenv.sh
+
+analyze_failure mypkg.vhdl
+
+clean
+
+echo "Test successful"
