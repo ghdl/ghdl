16229d80d577d320a600b32f67f4767d763dfc4d
Tristan Gingold <tgingold@free.fr>
2017-08-17 05:53:33 +0200

    travis: remove script deploy, adjust fold.

--- dist/linux/travis-ci.sh
+++ dist/linux/travis-ci.sh
@@ -76,9 +76,9 @@ t=0; for b in $blds; do
   workdir="../wrk-$t"
   # Display log (with travis log folding commands)
   printf "$ANSI_YELLOW[TRAVIS] Print BUILD $t log $ANSI_NOCOLOR\n"
-  echo "travis_fold:start:log-$t"
+  echo "travis_fold:start:log.$t"
   cat $workdir/log.log
-  echo "travis_fold:end:log-$t"
+  echo "travis_fold:end:log.$t"
 
   # Read the last line of the log
   RESULT="$(tail -1 $workdir/log.log)"
