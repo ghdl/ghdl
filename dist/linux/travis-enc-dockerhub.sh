5ca9bee36d81c34d16711efd4910c4ef3278fb3e
1138-4EB <1138-4EB@users.noreply.github.com>
2017-12-12 21:55:52 +0100

    use travis stages, use docker multistage builds

---
+++ dist/linux/travis-enc-dockerhub.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# - Run `$(command -v winpty) docker run --rm -it alpine sh -c "REPO='https://github.com/<user|organization>/<repo>'; USERNAME=<username>; PASSWORD=<password>; $(cat travis-enc.sh)"`
+# - Check the log and copy the secure key to `.travis.yml`: https://docs.travis-ci.com/user/environment-variables/#Defining-encrypted-variables-in-.travis.yml
+
+apk add -U --no-cache git openssh ruby ruby-dev libffi-dev build-base ruby-dev libc-dev libffi-dev linux-headers
+gem install travis --no-rdoc --no-ri
+
+git clone $REPO ./tmp-repo && cd tmp-repo
+
+travis login --org --auto
+travis encrypt DOCKER_USER=$USERNAME
+travis encrypt DOCKER_PASS=$PASSWORD
