82548be462091370f1fbd664c590bebe8f6f7096
Tristan Gingold <tgingold@free.fr>
2018-12-12 06:47:27 +0100

    Add more test cases for #646

--- testsuite/gna/issue646/testsuite.sh
+++ testsuite/gna/issue646/testsuite.sh
@@ -7,6 +7,16 @@ export GHDL_STD_FLAGS=--std=08
 analyze repro.vhdl
 elab_simulate repro
 
+# TODO: init value.
+#analyze repro2.vhdl
+#elab_simulate repro2
+
+analyze repro3.vhdl
+elab_simulate repro3
+
+analyze repro4.vhdl
+elab_simulate repro4
+
 clean
 
 echo "Test successful"
