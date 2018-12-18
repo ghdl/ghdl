9fd4c0216f7f576cfafad01e8842be40d3e1c475
Tristan Gingold <tgingold@free.fr>
2018-12-18 18:22:33 +0100

    sanity/005examples: do not try if python is not available.

--- testsuite/sanity/005examples/testsuite.sh
+++ testsuite/sanity/005examples/testsuite.sh
@@ -2,6 +2,12 @@
 
 . ../../testenv.sh
 
+# Skip the test if python is not available.
+if ! python -V > /dev/null 2>&1; then
+  echo "test skipped, python not found"
+  exit 0
+fi
+
 # Extract examples
 python extract_vhdl.py hello.vhdl heartbeat.vhdl adder.vhdl adder_tb.vhdl < ../../../doc/using/QuickStartGuide.rst
 
