7021fba68ae037edb0899e137785cbe3f5d9ac35
1138-4EB <1138-4EB@users.noreply.github.com>
2019-01-06 16:56:41 +0100

    Add man pages job to travis (#733)
    * add man pages job to travis

---
+++ dist/travis/man.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+rm -rf doc/_build/man/*
+
+set -e
+
+docker run --rm -it \
+  -v /$(pwd):/src \
+  -w //src/doc \
+  btdi/sphinx:py2-featured \
+  sh -c "sphinx-build -T -b man . ./_build/man"
+
+nroff -man doc/_build/man/ghdl.1
