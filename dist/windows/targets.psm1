0c9f43c60ac36f619ef7cea8a34745d24664c35e
Tristan Gingold <tgingold@free.fr>
2019-04-24 19:07:56 +0200

    grt: update windows targets.psm1 with grt-dynload.

--- dist/windows/targets.psm1
+++ dist/windows/targets.psm1
@@ -235,6 +235,7 @@ function Invoke-CompileCFiles
 	$SourceFiles = @()
 	$SourceFiles += New-Object PSObject -Property @{File="grt\grt-cbinding.c";			CFlags=@()}
 	$SourceFiles += New-Object PSObject -Property @{File="grt\grt-cvpi.c";					CFlags=@()}
+	$SourceFiles += New-Object PSObject -Property @{File="grt\grt-dynload.c";					CFlags=@()}
 	$SourceFiles += New-Object PSObject -Property @{File="grt\config\clock.c";			CFlags=@()}
 	$SourceFiles += New-Object PSObject -Property @{File="grt\config\win32.c";			CFlags=@('-DWITH_GNAT_RUN_TIME')}
 	$SourceFiles += New-Object PSObject -Property @{File="ortho\mcode\memsegs_c.c";	CFlags=@()}
@@ -314,6 +315,7 @@ function Invoke-CompileGHDLAdaFiles
 	$Parameters += 'grt-cbinding.o'
 	$Parameters += 'clock.o'
 	$Parameters += 'grt-cvpi.o'
+	$Parameters += 'grt-dynload.o'
 	$Parameters += 'memsegs_c.o'
 	$Parameters += 'win32.o'
 	$Parameters += '-ldbghelp'
