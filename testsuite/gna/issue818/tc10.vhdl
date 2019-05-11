b3e340b0fbef7e7043f9626ee6d37a69f10edfe8
Tristan Gingold <tgingold@free.fr>
2019-05-11 07:44:18 +0200

    Add testcases for #818

---
+++ testsuite/gna/issue818/tc10.vhdl
@@ -0,0 +1,17 @@
+entity tc10 is
+end;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+architecture behav of tc10 is
+  signal clk : std_logic;
+  signal tg : std_logic;
+begin
+  process (clk) is
+  begin
+    if ?? falling_edge(clk) and (?? tg) then
+      null;
+    end if;
+  end process;
+end behav;
