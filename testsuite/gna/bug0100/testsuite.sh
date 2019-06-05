1ae3bebbc7565975906805dcebe204cb17e7a121
Tristan Gingold <tgingold@free.fr>
2019-06-05 20:32:15 +0200

    Add a test for previous two commits.

--- testsuite/gna/bug0100/testsuite.sh
+++ testsuite/gna/bug0100/testsuite.sh
@@ -3,6 +3,13 @@
 . ../../testenv.sh
 
 analyze_failure --force-analysis inst1.vhdl
+analyze_failure --force-analysis notype1.vhdl
+
+if analyze_failure --force-analysis notype1.vhdl 2>&1 | grep -q "indexed name"; then
+  :
+else
+  echo "FAIL: missing error message from semantic analysis"
+fi
 
 clean
 
