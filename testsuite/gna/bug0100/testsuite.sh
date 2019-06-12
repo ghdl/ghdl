23bd5955da7d80e56e4bc9d1f756d13a1753d6e7
Tristan Gingold <tgingold@free.fr>
2019-06-12 18:41:48 +0200

    Add a testcase for previous commit.

--- testsuite/gna/bug0100/testsuite.sh
+++ testsuite/gna/bug0100/testsuite.sh
@@ -4,6 +4,7 @@
 
 analyze_failure --force-analysis inst1.vhdl
 analyze_failure --force-analysis notype1.vhdl
+analyze_failure --force-analysis notype2.vhdl
 
 if analyze_failure --force-analysis notype1.vhdl 2>&1 | grep -q "indexed name"; then
   :
