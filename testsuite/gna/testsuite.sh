a8e477a114b528866dfeb5146c9377f57bab604c
Tristan Gingold <tgingold@free.fr>
2019-08-14 07:34:46 +0200

    testsuite/gna: use common testsuite.sh

--- testsuite/gna/testsuite.sh
+++ testsuite/gna/testsuite.sh
@@ -1,21 +1,12 @@
 #! /bin/sh
 
-# Driver for the GNA testsuite.
-# Each test correspond to one report, and is put in one directory (using
-# the support/bug number)
-#
-# bug0XX is for bugs not reported.
-# bug[1-9]XXX is for bugs reported on https://gna.org/bugs/?group=ghdl
-# srXXX is for support reported on https://gna.org/support/?group=ghdl
-# debXX is for bugs report on https://www.debian.org/Bugs/
-# ticketXX is from https://sourceforge.net/p/ghdl-updates/tickets/
-# issueXXX is from https://github.com/ghdl/ghdl/issues
+# Driver for a testsuite.
 
 set -e
 
 # This is the only place where test dirs are specified.  Do not duplicate this
 # line
-dirs="bug* sr* deb* ticket* issue* gls*"
+dirs="*[0-9]"
 
 failures=""
 full=n
@@ -38,7 +29,7 @@ done
 
 singlerun() {
   echo ""
-  echo "GNA dir $1:"
+  echo "dir $1:"
   cd $1
   if ! ./testsuite.sh; then
     echo "#################################################################"
@@ -56,7 +47,7 @@ singlerun() {
 for i in $dirs; do singlerun $i $full; done
 
 if [ x"$failures" = x"" ]; then
-    echo "GNA tests are successful" && exit 0
+    echo "tests are successful" && exit 0
 else
-    echo "GNA test failed ($failures)" && exit 1
+    echo "test failed ($failures)" && exit 1
 fi
