c337a3dc2e3b5d635f957bbd11a42ab69d9a0aaf
Tristan Gingold <tgingold@free.fr>
2019-10-03 18:45:43 +0200

    testsuite/synth: add a testcase for previous commit.

---
+++ testsuite/synth/issue953/ent2.vhdl
@@ -0,0 +1,8 @@
+entity ent2 is
+end;
+
+architecture a of ent2 is
+    signal x : integer;
+    signal y : integer := x / 2;
+begin
+end;
--- testsuite/synth/issue953/testsuite.sh
+++ testsuite/synth/issue953/testsuite.sh
@@ -2,7 +2,7 @@
 
 . ../../testenv.sh
 
-for f in ent; do
+for f in ent ent2; do
   synth $f.vhdl -e $f > syn_$f.vhdl
 #  analyze syn_$f.vhdl
 done
