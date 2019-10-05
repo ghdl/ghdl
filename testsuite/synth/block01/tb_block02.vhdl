5c72f37c3eaf69d59f0f9a22473d68ce70858450
Tristan Gingold <tgingold@free.fr>
2019-10-05 08:05:26 +0200

    testsuite/synth: add a test for block declarations.

---
+++ testsuite/synth/block01/tb_block02.vhdl
@@ -0,0 +1,40 @@
+entity tb_block02 is
+end tb_block02;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+architecture behav of tb_block02 is
+  signal clk : std_logic;
+  signal din : std_logic;
+  signal dout : std_logic;
+begin
+  dut: entity work.block02
+    port map (
+      q => dout,
+      d => din,
+      clk => clk);
+
+  process
+    procedure pulse is
+    begin
+      clk <= '0';
+      wait for 1 ns;
+      clk <= '1';
+      wait for 1 ns;
+    end pulse;
+  begin
+    din <= '0';
+    pulse;
+    assert dout = '0' severity failure;
+    din <= '1';
+    pulse;
+    assert dout = '1' severity failure;
+    pulse;
+    assert dout = '1' severity failure;
+    din <= '0';
+    pulse;
+    assert dout = '0' severity failure;
+    wait;
+  end process;
+end behav;
