4c014bcc9e319eb6078d6f21857cd170c26483f1
Tristan Gingold <tgingold@free.fr>
2019-11-06 04:49:21 +0100

    testsuite/synth: add a similar testcase for #1009

--- testsuite/synth/issue1009/testsuite.sh
+++ testsuite/synth/issue1009/testsuite.sh
@@ -5,6 +5,9 @@
 synth record_test.vhdl -e > syn_record_test.vhdl
 analyze syn_record_test.vhdl
 
+synth array_test.vhdl -e > syn_array_test.vhdl
+analyze syn_array_test.vhdl
+
 clean
 
 echo "Test successful"
