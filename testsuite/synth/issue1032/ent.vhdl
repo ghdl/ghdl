e50321edd0f9790b7a6c85179979dfe29790580e
Tristan Gingold <tgingold@free.fr>
2019-11-26 19:24:29 +0100

    testsuite/synth: add testcase for #1032

---
+++ testsuite/synth/issue1032/ent.vhdl
@@ -0,0 +1,7 @@
+entity ent is
+end ent;
+
+architecture a of ent is
+	constant time_div : time := 20 ns / 2;
+begin
+end a;
---
+++ testsuite/synth/issue1032/syn_ent.vhdl
@@ -0,0 +1,10 @@
+entity ent is
+end ent;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+architecture rtl of ent is
+begin
+end rtl;
