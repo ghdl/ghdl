2bb61759d873dcffc8943f667073d472ca21a6dc
Tristan Gingold <tgingold@free.fr>
2019-11-30 10:25:21 +0100

    testsuite: add some tests for #1044

---
+++ testsuite/synth/issue1044/ent.vhdl
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ent is
+	generic (
+		VAL : real := 1.5
+	);
+	port (
+		lt  : out std_logic
+	);
+end;
+
+architecture a of ent is
+   constant fmul : real := val * 5.0;
+   constant fneg : real := -val;
+begin
+	lt  <= '1' when VAL  < 1.5 else '0';
+end;
+
