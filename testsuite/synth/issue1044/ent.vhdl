d11ad2282157564dad9e53eccf9f2ec8a05bbda7
Tristan Gingold <tgingold@free.fr>
2019-12-01 08:57:39 +0100

    testcase: complete test for #1044.

--- testsuite/synth/issue1044/ent.vhdl
+++ testsuite/synth/issue1044/ent.vhdl
@@ -13,6 +13,9 @@ end;
 architecture a of ent is
    constant fmul : real := val * 5.0;
    constant fneg : real := -val;
+   constant fid : real := +val;
+   constant fabs : real := abs val;
+   constant fexp : real := val ** 2;
 begin
 	lt  <= '1' when VAL  < 1.5 else '0';
 end;
