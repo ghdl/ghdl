c0da72264b38b6b6e7757b00f01d1d355fdf4d9e
Tristan Gingold <tgingold@free.fr>
2020-01-27 07:56:20 +0100

    testsuite/gna: add more tests for #1038

--- testsuite/gna/issue1038/testsuite.sh
+++ testsuite/gna/issue1038/testsuite.sh
@@ -9,8 +9,14 @@ elab_simulate repro
 analyze repro2.vhdl
 elab_simulate repro2
 
+analyze repro3.vhdl
+elab_simulate repro3
+
 analyze repro4.vhdl
 elab_simulate repro4
+
+analyze repro5.vhdl
+elab_simulate repro5
 
 clean
 
