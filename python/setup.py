266a237f7708bf57638a9973a8a6757ca28b4d80
Tristan Gingold <tgingold@free.fr>
2020-03-09 18:19:48 +0100

    python: update setup.py to install ghdl-ls

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 import re
 
 def get_version():
-    # Try from version.py.  Reads it to avoid to load the shared library.
+    # Try from version.py.  Reads it to avoid loading the shared library.
     r = re.compile("^__version__ = '(.*)'\n")
     try:
         l = open('libghdl/version.py').read()
@@ -20,23 +20,30 @@ def get_version():
 version=get_version()
 
 setup(
-    name='libghdl',
+    name='pyghdl',
     version=version,
-    description='Interface to ghdl, a VHDL analyzer',
-    long_description="""GHDL is a vhdl simulator and libghdl provides a low-level
-interface to the parser. This library gives access to the AST so that you can
-write tools like linters.
-""",
+    description='VHDL Language Server and interface to ghdl, a VHDL analyzer',
+    long_description=open('README').read(),
     author='Tristan Gingold',
     author_email='tgingold@free.fr',
     url='http://github.com/ghdl/ghdl',
     license='GPL-2.0-or-later',
     package_dir={
-        'libghdl': './libghdl'
+        'libghdl': 'libghdl',
+        'vhdl_langserver': 'vhdl_langserver'
     },
     packages=[
         'libghdl',
         'libghdl.thin',
-        'libghdl.thin.vhdl'
-    ]
+        'libghdl.thin.vhdl',
+        'vhdl_langserver'
+    ],
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'ghdl-ls = vhdl_langserver.main:main',
+        ]
+    },
 )
