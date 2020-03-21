79af4ca95f6887309bdcb21fece4704d6e1776c3
Tristan Gingold <tgingold@free.fr>
2020-03-21 08:22:37 +0100

    python/setup.py: add missing dependency.

--- python/setup.py
+++ python/setup.py
@@ -38,6 +38,12 @@ setup(
         'libghdl.thin.vhdl',
         'vhdl_langserver'
     ],
+    # List run-time dependencies here. For an analysis of "install_requires"
+    # vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'attrs'
+    ],
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
