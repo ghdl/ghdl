8982646305a71ab14057f84e80ee504c8b7d2c67
Tristan Gingold <tgingold@free.fr>
2020-03-23 19:03:17 +0100

    testsuite/synth: add a test for #1172

---
+++ testsuite/synth/issue1172/test_module.vhdl
@@ -0,0 +1,7 @@
+entity test_module is
+end entity test_module;
+
+architecture rtl of test_module is
+  constant CONSTANT1, CONSTANT2 : integer := 4;
+begin
+end architecture rtl;
---
+++ testsuite/synth/issue1172/testsuite.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+. ../../testenv.sh
+
+synth_analyze test_module
+clean
+
+echo "Test successful"
