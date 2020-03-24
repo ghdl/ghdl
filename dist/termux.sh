480d873446bcc122f0fa587ce1a3c773325f068a
umarcor <unai.martinezcorral@ehu.eus>
2020-03-24 15:31:48 +0100

    termux: setting symlinks explicitly is not required now (its-pointless/gcc_termux#84)

--- dist/termux.sh
+++ dist/termux.sh
@@ -5,8 +5,6 @@ cd $(dirname "$0")/..
 curl -fsSL https://its-pointless.github.io/setup-pointless-repo.sh | bash -
 pkg install gnat-9
 setupgcc-9
-ln -s $PREFIX/lib/gcc/aarch64-linux-android/9.2.0/libgnat-9.so $PREFIX/lib/libgnat-9.so
-ln -s $PREFIX/lib/gcc/aarch64-linux-android/9.2.0/libgnarl-9.so $PREFIX/lib/libgnarl-9.so
 
 mkdir -p build-termux
 cd build-termux
