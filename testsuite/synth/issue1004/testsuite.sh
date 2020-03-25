2f8ca2c8e452c3321386a6be04ae24d5369fe148
Tristan Gingold <tgingold@free.fr>
2020-03-25 18:45:30 +0100

    Rewrite harness of a test.

--- testsuite/synth/issue1004/testsuite.sh
+++ testsuite/synth/issue1004/testsuite.sh
@@ -3,21 +3,9 @@
 . ../../testenv.sh
 
 GHDL_STD_FLAGS=--std=08
-for t in test; do
-  synth $t.vhdl -e $t > syn_$t.vhdl
-  analyze syn_$t.vhdl
-done
+synth_analyze test
 
-for t in test2; do
-    analyze $t.vhdl tb_$t.vhdl
-    elab_simulate tb_$t
-    clean
-
-    synth $t.vhdl -e $t > syn_$t.vhdl
-    analyze syn_$t.vhdl tb_$t.vhdl
-    elab_simulate tb_$t --ieee-asserts=disable-at-0
-    clean
-done
+synth_tb test2
 
 clean
 
