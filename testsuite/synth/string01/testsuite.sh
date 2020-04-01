c23324b6b718eda1b34302ea497e0f65fca1a9e1
Tristan Gingold <tgingold@free.fr>
2020-04-01 23:12:42 +0200

    testsuite/synth: use harness.

--- testsuite/synth/string01/testsuite.sh
+++ testsuite/synth/string01/testsuite.sh
@@ -2,16 +2,6 @@
 
 . ../../testenv.sh
 
-analyze string01.vhdl tb_string01.vhdl
-elab_simulate tb_string01
-
-clean
-
-synth string01.vhdl -e string01 > syn_string01.vhdl
-
-analyze syn_string01.vhdl tb_string01.vhdl
-elab_simulate tb_string01
-
-clean
+synth_tb string01
 
 echo "Test successful"
