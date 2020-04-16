6bc5cf204e74958e9e0d1ce0c4aea788d5fd58ba
Tristan Gingold <tgingold@free.fr>
2020-04-16 19:25:54 +0200

    testsuite/gna: simplify test #1229

--- testsuite/gna/issue1229/testsuite.sh
+++ testsuite/gna/issue1229/testsuite.sh
@@ -2,14 +2,14 @@
 
 . ../../testenv.sh
 
-gcc -c -fPIC aux.c
-gcc -shared -o aux.so aux.o
+# gcc -c -fPIC aux.c
+# gcc -shared -o aux.so aux.o
 
 export GHDL_STD_FLAGS=--std=08
 analyze pkg.vhdl tb.vhdl
-elab_simulate tb
+# elab_simulate tb
 
 clean
-rm -f aux.o aux.so
+# rm -f aux.o aux.so
 
 echo "Test successful"
