628c01a31db7f92f4fd7476f7adf3e9a136bc5de
Tristan Gingold <tgingold@free.fr>
2020-04-18 09:27:31 +0200

    testsuite/gna: complete test #1240

--- testsuite/gna/issue1240/testsuite.sh
+++ testsuite/gna/issue1240/testsuite.sh
@@ -2,6 +2,14 @@
 
 . ../../testenv.sh
 
+analyze issue1.vhdl
+elab_simulate_failure issue1
+
+analyze issue2.vhdl
+elab_simulate_failure issue2
+
+clean
+
 export GHDL_STD_FLAGS=-Werror=runtime-error
 
 analyze_failure issue1.vhdl
