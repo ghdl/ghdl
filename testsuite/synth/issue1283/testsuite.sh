54cd0b5bd2cb29668fc41946a95bba0d13a67199
Tristan Gingold <tgingold@free.fr>
2020-05-12 19:01:19 +0200

    testsuite/synth: adjust test #1283

--- testsuite/synth/issue1283/testsuite.sh
+++ testsuite/synth/issue1283/testsuite.sh
@@ -4,9 +4,9 @@
 
 GHDL_STD_FLAGS=--std=08
 synth_analyze issue1
-synth_failure issue2
-synth_failure issue3
-synth_failure issue4
+synth_failure issue2.vhdl -e
+synth_failure issue3.vhdl -e
+synth_failure issue4.vhdl -e
 
 clean
 
