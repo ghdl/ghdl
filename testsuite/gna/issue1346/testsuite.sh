18e67551280da85b34e840aa60e5454b01e66b79
Tristan Gingold <tgingold@free.fr>
2020-06-02 03:24:21 +0200

    testsuite/gna: adjust issue1346

--- testsuite/gna/issue1346/testsuite.sh
+++ testsuite/gna/issue1346/testsuite.sh
@@ -4,7 +4,8 @@
 
 export GHDL_STD_FLAGS=--std=08
 
-analyze_failure wb_pkg.vhd wb_rst_pkg.vhd memory_map_pkg.vhd memory_map.vhd 
+analyze wb_pkg.vhd wb_rst_pkg.vhd memory_map_pkg.vhd
+analyze_failure memory_map.vhd
 
 clean
 
