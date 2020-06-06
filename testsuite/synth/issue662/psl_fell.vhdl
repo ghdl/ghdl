dd6a99f4b0951bed372a868c8174683c10a187f0
tmeissner <programming@goodcleanfun.de>
2020-06-06 19:52:50 +0200

    testsuite/synth: add a test of PSL built-in function fell() for #662

---
+++ testsuite/synth/issue662/psl_fell.vhdl
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity psl_fell is
+  port (clk, a, b : in std_logic
+  );
+end entity psl_fell;
+
+
+architecture psl of psl_fell is
+begin
+
+  -- All is sensitive to rising edge of clk
+  default clock is rising_edge(clk);
+
+  -- This assertion holds
+  FELL_0_a : assert always {a; not a} |-> fell(a);
+
+  -- This assertion holds
+  FELL_1_a : assert always (fell(a) -> (prev(a) = '1' and a = '0'));
+
+  -- This assertion should fail at cycle 11
+  FELL_2_a : assert always fell(a) -> b;
+
+end architecture psl;
