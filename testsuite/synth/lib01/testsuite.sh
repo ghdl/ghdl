0fc1b65f6c0182d91ff56946944f86ceeef482ea
Tristan Gingold <tgingold@free.fr>
2020-07-01 21:13:11 +0200

    testsuite/synth: improve test after previous commit.

--- testsuite/synth/lib01/testsuite.sh
+++ testsuite/synth/lib01/testsuite.sh
@@ -6,8 +6,10 @@ analyze --work=mylib and2.vhdl
 analyze and3.vhdl tb_and3.vhdl
 elab_simulate tb_and3
 clean
+clean mylib
 
-synth --work=mylib and2.vhdl --work=work and3.vhdl -e and3 > syn_and3.vhdl
+synth --work=work and3.vhdl --work=mylib and2.vhdl --work=work -e and3 > syn_and3.vhdl
+analyze --work=mylib and2.vhdl
 analyze syn_and3.vhdl tb_and3.vhdl
 elab_simulate tb_and3
 clean
