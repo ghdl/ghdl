b3aec78254d4048f61693a88a3d25d1f5d8ccd25
Tristan Gingold <tgingold@free.fr>
2020-07-02 18:54:37 +0200

    testsuite/synth: add a test for signals in packages.

---
+++ testsuite/synth/issue1387/repro1.vhdl
@@ -0,0 +1,15 @@
+package repro1_pkg is
+  signal s : bit;
+end;
+
+use work.repro1_pkg.all;
+
+entity repro1 is
+  port (a,b : bit;
+        c : out bit);
+end repro1;
+
+architecture behav of repro1 is
+begin
+  c <= a xor b;
+end behav;
---
+++ testsuite/synth/issue1387/testsuite.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+. ../../testenv.sh
+
+synth_failure repro1.vhdl -e
+
+echo "Test successful"
