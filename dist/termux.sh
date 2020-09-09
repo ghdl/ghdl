3b5ec33830c238b9cd510c98be9dc65a9274df9a
umarcor <unai.martinezcorral@ehu.eus>
2020-09-09 11:26:37 +0200

    termux: ln gnatmake-10

--- dist/termux.sh
+++ dist/termux.sh
@@ -6,6 +6,9 @@ curl -fsSL https://its-pointless.github.io/setup-pointless-repo.sh | bash -
 pkg install gnat-10 llvm make
 setupgcc-10
 
+# Temporal fix. See https://github.com/its-pointless/gcc_termux/issues/100
+ln -s $PREFIX/bin/gnatmake-10 $PREFIX/bin/gnatmake
+
 mkdir -p build-termux
 cd build-termux
 ../configure --default-pic --enable-synth --with-llvm-config=llvm-config --prefix="$PREFIX"
