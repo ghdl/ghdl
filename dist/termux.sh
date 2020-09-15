8cfff809237f61e1651842def11d69af2d86a917
umarcor <unai.martinezcorral@ehu.eus>
2020-09-15 03:11:57 +0200

    termux: use setup-pathforgcc and clang++
    Co-authored-by: Benjamin Renz <BenjaminRenz@users.noreply.github.com>

--- dist/termux.sh
+++ dist/termux.sh
@@ -3,14 +3,13 @@
 cd $(dirname "$0")/..
 
 curl -fsSL https://its-pointless.github.io/setup-pointless-repo.sh | bash -
-pkg install gnat-10 llvm make
+pkg install gnat-10 llvm clang make
 setupgcc-10
-
-# Temporal fix. See https://github.com/its-pointless/gcc_termux/issues/100
-ln -s $PREFIX/bin/gnatmake-10 $PREFIX/bin/gnatmake
+setup-patchforgcc
 
 mkdir -p build-termux
 cd build-termux
+CXX=clang++
 ../configure --default-pic --enable-synth --with-llvm-config=llvm-config --prefix="$PREFIX"
 make
 make install
