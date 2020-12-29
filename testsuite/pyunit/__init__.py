e86f303af5e955e6827be8b232cd877182396b70
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2020-12-29 23:02:37 +0100

    Prepare pyunit tests for parallel and independent execution with pytest/pytest-forked.

--- testsuite/pyunit/__init__.py
+++ testsuite/pyunit/__init__.py
@@ -1,14 +1,14 @@
-from unittest import TestSuite
-
-try:
-	from testsuite.pyunit import libghdl, dom
-except ModuleNotFoundError:
-	from pyunit import libghdl, dom
-
-def load_tests(loader, testCases, pattern):
-	suite = TestSuite()
-
-	suite.addTests(loader.loadTestsFromModule(libghdl))
-	suite.addTests(loader.loadTestsFromModule(dom))
-
-	return suite
+# from unittest import TestSuite
+#
+# try:
+# 	from testsuite.pyunit import libghdl, dom
+# except ModuleNotFoundError:
+# 	from pyunit import libghdl, dom
+#
+# def load_tests(loader, testCases, pattern):
+# 	suite = TestSuite()
+#
+# 	suite.addTests(loader.loadTestsFromModule(libghdl))
+# 	suite.addTests(loader.loadTestsFromModule(dom))
+#
+# 	return suite
--- testsuite/pyunit/dom/__init__.py
+++ testsuite/pyunit/dom/__init__.py
@@ -1,13 +1,13 @@
-from unittest import TestSuite
-
-try:
-	from testsuite.pyunit.dom import SimpleEntity
-except ModuleNotFoundError:
-	from pyunit.dom import SimpleEntity
-
-def load_tests(loader, testCases, pattern):
-	suite = TestSuite()
-
-	suite.addTests(loader.loadTestsFromModule(SimpleEntity))
-
-	return suite
+# from unittest import TestSuite
+#
+# try:
+# 	from testsuite.pyunit.dom import SimpleEntity_test
+# except ModuleNotFoundError:
+# 	from pyunit.dom import SimpleEntity
+#
+# def load_tests(loader, testCases, pattern):
+# 	suite = TestSuite()
+#
+# 	suite.addTests(loader.loadTestsFromModule(SimpleEntity_test))
+#
+# 	return suite
