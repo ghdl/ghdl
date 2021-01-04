49bb019a9f57e52397c5f1b2bbfb20cd83645231
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-01-05 00:07:53 +0100

    Run AutoProgram to extract ghdl-ls CLI options. Add a link to gnatdoc.

--- doc/conf.py
+++ doc/conf.py
@@ -61,7 +61,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
 	"_build",
-	"_themes",
+	"_theme",
 	"Thumbs.db",
 	".DS_Store"
 ]
