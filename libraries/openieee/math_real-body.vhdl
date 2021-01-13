4868294436574660552ccef50a5b0849559393de
umarcor <unai.martinezcorral@ehu.eus>
2021-01-13 03:07:10 +0100

    update license headers

--- libraries/openieee/math_real-body.vhdl
+++ libraries/openieee/math_real-body.vhdl
@@ -2,19 +2,18 @@
 --  IEEE 1076.2 math_real package body.
 --  Copyright (C) 2015-2021 Tristan Gingold
 --
---  GHDL is free software; you can redistribute it and/or modify it under
---  the terms of the GNU General Public License as published by the Free
---  Software Foundation; either version 2, or (at your option) any later
---  version.
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 2 of the License, or
+--  (at your option) any later version.
 --
---  GHDL is distributed in the hope that it will be useful, but WITHOUT ANY
---  WARRANTY; without even the implied warranty of MERCHANTABILITY or
---  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
---  for more details.
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
 --
 --  You should have received a copy of the GNU General Public License
---  along with GHDL; see the file COPYING.md.  If not see
---  <http://www.gnu.org/licenses/>.
+--  along with this program.  If not, see <gnu.org/licenses>.
 
 package body MATH_REAL is
   function SIGN (X : REAL) return REAL is
--- libraries/openieee/v87/numeric_std.vhdl
+++ libraries/openieee/v87/numeric_std.vhdl
@@ -2,19 +2,18 @@
 --  IEEE 1076.3 compliant numeric std package.
 --  Copyright (C) 2015 Tristan Gingold
 --
---  GHDL is free software; you can redistribute it and/or modify it under
---  the terms of the GNU General Public License as published by the Free
---  Software Foundation; either version 2, or (at your option) any later
---  version.
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 2 of the License, or
+--  (at your option) any later version.
 --
---  GHDL is distributed in the hope that it will be useful, but WITHOUT ANY
---  WARRANTY; without even the implied warranty of MERCHANTABILITY or
---  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
---  for more details.
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
 --
 --  You should have received a copy of the GNU General Public License
---  along with GCC; see the file COPYING2.  If not see
---  <http://www.gnu.org/licenses/>.
+--  along with this program.  If not, see <gnu.org/licenses>.
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
