786d1bfbb1dec256ef375da06089fed8f9885b78
umarcor <unai.martinezcorral@ehu.eus>
2021-01-16 07:31:48 +0100

    update version/copyright year

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 #
 # License:
 # ============================================================================
-#  Copyright (C) 2019-2020 Tristan Gingold
+#  Copyright (C) 2019-2021 Tristan Gingold
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
