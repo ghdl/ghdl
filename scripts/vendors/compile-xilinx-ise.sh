34a096229ccadf895b2130bafa417e869c437b3c
Adrian Byszuk <adrian.byszuk@gmail.com>
2021-02-17 15:15:14 +0100

    vendors: support deprecated param names (#1653)
    Co-authored-by: Adrian Byszuk <adrian.byszuk@cern.ch>

--- scripts/vendors/compile-xilinx-ise.sh
+++ scripts/vendors/compile-xilinx-ise.sh
@@ -121,11 +121,11 @@ while [[ $# -gt 0 ]]; do
 			GHDL="$2"				# overwrite a potentially existing GHDL environment variable
 			shift						# skip argument
 			;;
-		--source)
+		-src|--source)
 			SrcDir="$2"
 			shift						# skip argument
 			;;
-		--output)
+		-out|--output)
 			DestDir="$2"
 			shift						# skip argument
 			;;
@@ -173,9 +173,9 @@ if [[ $COMMAND -le 1 ]]; then
 	echo "  -H --halt-on-error           Halt on error(s)."
 	echo ""
 	echo "Advanced options:"
-	echo "     --ghdl <GHDL binary>      Path to GHDL's executable, e.g. /usr/local/bin/ghdl"
-	echo "     --output <dir name>       Name of the output directory, e.g. ise"
-	echo "     --source <Path to ISE>    Path to the sources."
+	echo "  --ghdl <GHDL binary>         Path to GHDL's executable, e.g. /usr/local/bin/ghdl"
+	echo "  --output <dir name>          Name of the output directory, e.g. ise"
+	echo "  --source <Path to ISE>       Path to the sources."
 	echo ""
 	echo "Verbosity:"
 	echo "  -v --verbose                 Print verbose messages."
