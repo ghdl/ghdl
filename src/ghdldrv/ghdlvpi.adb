2036d713a73a91a8014d0d63201d795807e0c924
Tristan Gingold <tgingold@free.fr>
2021-03-07 21:22:36 +0100

    ghdlvpi.adb: use Directory_Separator.

--- src/ghdldrv/ghdlvpi.adb
+++ src/ghdldrv/ghdlvpi.adb
@@ -37,7 +37,9 @@ package body Ghdlvpi is
       --  Compute install path
       Ghdllocal.Set_Exec_Prefix_From_Program_Name;
 
-      return Ghdllocal.Exec_Prefix.all & Directory_Separator & "include/ghdl";
+      return Ghdllocal.Exec_Prefix.all
+        & Directory_Separator & "include"
+        & Directory_Separator & "ghdl";
    end Get_Vpi_Include_Dir;
 
    --  Return the lib directory.
