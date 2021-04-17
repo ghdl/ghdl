bd2955b3203d205a54ba3dbdb22016f22c79d4a0
Marlon James <marlon.james@gmail.com>
2021-04-17 15:08:50 -0700

    VHPI: add tracing tests

---
+++ testsuite/vhpi/005trace_file/vhpi_lib.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <vhpi_user.h>
+
+
+void my_callback(const vhpiCbDataT * cb_data) {
+}
+
+void my_startup()
+{
+  printf ("VHPI lib\n");
+
+  vhpiCbDataT cb_data;
+  vhpiTimeT time;
+
+  cb_data.reason = vhpiCbStartOfSimulation;
+  cb_data.cb_rtn = my_callback;
+  cb_data.obj = NULL;
+  cb_data.time = &time;
+  cb_data.value = NULL;
+  cb_data.user_data = (char *)NULL;
+  time.high = 0;
+  time.low = 0;
+
+  vhpi_register_cb(&cb_data, NULL);
+}
+
+void (*vhpi_startup_routines[]) () =
+{
+  my_startup,
+  0
+};
