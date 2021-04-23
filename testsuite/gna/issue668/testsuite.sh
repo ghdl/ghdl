ce49befde5e95066ee34469e043a48d94423c53b
Xiretza <xiretza@xiretza.xyz>
2021-04-23 20:59:04 +0200

    GHW: handle unbounded record subtypes (#1743) Fix #1740

--- testsuite/gna/issue668/testsuite.sh
+++ testsuite/gna/issue668/testsuite.sh
@@ -3,15 +3,15 @@
 . ../../testenv.sh
 
 export GHDL_STD_FLAGS=--std=08
-for item in wb_demux_tb repro2; do
-  analyze "$item".vhdl
-  elab "$item"
+for f in wb_demux_tb repro2; do
+  analyze "$f".vhdl
+  elab "$f"
 
-  if ghdl_has_feature "$item" ghw; then
-    elab_simulate "$item" --dump-rti
-    elab_simulate "$item" --wave="$item".ghw
-    ghw_diff "$item"
-    rm -f "$item".txt "$item".ghw
+  if ghdl_has_feature "$f" ghw; then
+    elab_simulate "$f" --dump-rti
+    elab_simulate "$f" --wave="$f".ghw
+    ghw_diff "$f"
+    rm -f "$f".txt "$f".ghw
   fi
 done
 
