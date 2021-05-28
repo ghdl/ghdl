5885c15733efd56a0254b3735deceadf26f7d5d2
Tristan Gingold <tgingold@free.fr>
2021-05-28 20:57:09 +0200

    testsuite/gna: add a test for previous commit

---
+++ testsuite/gna/bug0100/name1.vhdl
@@ -0,0 +1,11 @@
+entity crash1 is
+end crash1;
+
+architecture behav of crash1 is
+  signal samples: bit;
+begin
+  process
+  begin
+    bit'(samples));
+  end process;
+end behav;
---
+++ testsuite/gna/bug0100/name2.vhdl
@@ -0,0 +1,11 @@
+entity name2 is
+end name2;
+
+architecture behav of name2 is
+  signal samples: bit;
+begin
+  process
+  begin
+    bit'samples);
+  end process;
+end behav;
