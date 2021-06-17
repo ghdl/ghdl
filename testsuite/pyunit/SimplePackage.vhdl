e5891f3900f5e58df37d32c2c18a0f339c094cf4
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-06-17 22:55:04 +0200

    Reduced testcase.

--- testsuite/pyunit/SimplePackage.vhdl
+++ testsuite/pyunit/SimplePackage.vhdl
@@ -3,20 +3,11 @@ use     ieee.std_logic_1164.all;
 use     ieee.numeric_std.all;
 
 package pack_1 is
-	constant const_1 : boolean;
+	constant const_1 : boolean := false;
 
-	type matrix is array(natural range <>, natural range <>) of std_logic;
-
-	subtype matrix8x8 is matrix(7 downto 0, 7 downto 0);
-
-	function func1(value : unsigned) return natural;
 end package;
 
 package body pack_1 is
 	constant const_1 : boolean := true;
 
-	function func1(value : unsigned) return natural is
-	begin
-		return to_integer(value);
-	end function;
 end package body;
