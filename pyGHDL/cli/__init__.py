8e2be80f43a2bdc4f4b84bb1ebd022df1bc65673
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-06-17 13:12:52 +0200

    Added a simple frontend for testing.

--- pyGHDL/cli/__init__.py
+++ pyGHDL/cli/__init__.py
@@ -1,20 +1,20 @@
 # =============================================================================
-#                ____ _   _ ____  _           _ _
-#   _ __  _   _ / ___| | | |  _ \| |      ___| (_)
-#  | '_ \| | | | |  _| |_| | | | | |     / __| | |
-#  | |_) | |_| | |_| |  _  | |_| | |___ | (__| | |
-#  | .__/ \__, |\____|_| |_|____/|_____(_)___|_|_|
-#  |_|    |___/
+#               ____ _   _ ____  _           _ _
+#  _ __  _   _ / ___| | | |  _ \| |      ___| (_)
+# | '_ \| | | | |  _| |_| | | | | |     / __| | |
+# | |_) | |_| | |_| |  _  | |_| | |___ | (__| | |
+# | .__/ \__, |\____|_| |_|____/|_____(_)___|_|_|
+# |_|    |___/
 # =============================================================================
-#  Authors:
-#    Patrick Lehmann
-#    Unai Martinez-Corral
+# Authors:
+#   Patrick Lehmann
+#   Unai Martinez-Corral
 #
 # Package package:  Package for command line interfaces.
 #
 # License:
 # ============================================================================
-#  Copyright (C) 2019-2020 Tristan Gingold
+#  Copyright (C) 2019-2021 Tristan Gingold
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
