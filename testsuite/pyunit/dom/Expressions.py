89f835733c4019c5cb087885a874f34cf4ff183d
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-06-19 03:03:29 +0200

    Testcase(s) for expressions.

--- testsuite/pyunit/dom/Expressions.py
+++ testsuite/pyunit/dom/Expressions.py
@@ -2,10 +2,11 @@ from pathlib  import Path
 from textwrap import dedent
 from unittest import TestCase
 
+from pyGHDL.dom import Expression
 from pyGHDL.dom.Misc       import Design, Document
+from pyGHDL.dom.Symbol import SimpleObjectSymbol
 from pyGHDL.dom.Object import Constant
-from pyGHDL.dom.Literal import IntegerLiteral
-
+from pyGHDL.dom.Expression import InverseExpression
 
 if __name__ == "__main__":
     print("ERROR: you called a testcase declaration file as an executable module.")
@@ -13,21 +14,17 @@ if __name__ == "__main__":
     exit(1)
 
 
-class Literals(TestCase):
+class Expressions(TestCase):
     _root = Path(__file__).resolve().parent.parent
 
-    def test_IntegerLiteral(self):
+    def test_NotExpression(self):
         self._filename: Path = self._root / "{className}.vhdl".format(className=self.__class__.__name__)
 
         sourceCode = dedent("""\
             package package_1 is
-              constant c0 : integer := 0;
-              constant c1 : integer := 1;
-              constant c2 : integer := 1024;
-              constant c3 : integer := 1048576;
+              constant c0 : boolean := not true;
             end package;
             """)
-        expected = (0, 1, 1024, 1048576)
 
         with self._filename.open(mode="w", encoding="utf-8") as file:
             file.write(sourceCode)
@@ -39,11 +36,14 @@ class Literals(TestCase):
         self.assertEqual(len(design.Documents[0].Packages), 1)
         package = design.Documents[0].Packages[0]
         self.assertTrue(package.Name == "package_1")
-        self.assertEqual(len(package.DeclaredItems), len(expected))
-        for i in range(len(expected)):
-            item: Constant = package.DeclaredItems[i]
-            self.assertTrue(isinstance(item, Constant))
-            self.assertTrue(item.Name == "c{}".format(i))
-            self.assertTrue(item.SubType.SymbolName == "integer")
-            self.assertTrue(isinstance(item.DefaultExpression, IntegerLiteral))
-            self.assertTrue(item.DefaultExpression.Value == expected[i])
+        self.assertEqual(len(package.DeclaredItems), 1)
+
+        item: Constant = package.DeclaredItems[0]
+        self.assertTrue(isinstance(item, Constant))
+        self.assertTrue(item.Name == "c0")
+        self.assertTrue(item.SubType.SymbolName == "boolean")
+
+        default: Expression = item.DefaultExpression
+        self.assertTrue(isinstance(default, InverseExpression))
+        self.assertTrue(isinstance(default.Operand, SimpleObjectSymbol))
+        self.assertTrue(default.Operand.SymbolName == "true")
