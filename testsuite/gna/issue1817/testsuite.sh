44f80dde6d0e248d850f3ca48e5167920f93ca92
Tristan Gingold <tgingold@free.fr>
2021-07-23 07:15:30 +0200

    testsuite/gna: add traces to investigate CI failures for #1817

--- testsuite/gna/issue1817/testsuite.sh
+++ testsuite/gna/issue1817/testsuite.sh
@@ -5,6 +5,8 @@
 GHDL_STD_FLAGS=--std=08
 
 if $GHDL --version | grep -q "GCC back-end code"; then
+    echo "GCC backend"
+    set -x
     is_gcc=true
 else
     is_gcc=false
@@ -20,6 +22,7 @@ analyze full_adder_tb.vhdl
 # Do not try to elaborate, libgcov may not be available
 
 if [ "$is_gcc" = true ]; then
+    ls -l
     test -f full_adder.gcno
     test -f full_adder_tb.gcno
 fi
