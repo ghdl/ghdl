163b5a804be113543276199ee36c193d7df75c8c
Tristan Gingold <tgingold@free.fr>
2021-09-07 18:14:25 +0200

    testsuite/synth: improve test for keep attribute

--- testsuite/synth/synth154/testsuite.sh
+++ testsuite/synth/synth154/testsuite.sh
@@ -7,4 +7,10 @@ synth_analyze keep
 grep -q "signal a : " syn_keep.vhdl
 clean
 
+synth -Werror keep2.vhdl -e > syn_keep2.vhdl
+grep -q "signal a : " syn_keep2.vhdl
+clean
+
+synth_failure -Werror err1.vhdl -e
+
 echo "Test successful"
