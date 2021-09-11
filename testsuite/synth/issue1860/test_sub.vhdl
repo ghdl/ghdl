cf57ad79e2ba7995df7e2af2d3ab7b94e040ae52
Tristan Gingold <tgingold@free.fr>
2021-09-11 19:20:02 +0200

    testsuite/synth: add a test for #1860

---
+++ testsuite/synth/issue1860/test_sub.vhdl
@@ -0,0 +1,6 @@
+ENTITY test_formal_test IS
+END ENTITY;
+
+ARCHITECTURE formal OF test_formal_test IS
+BEGIN
+END ARCHITECTURE;
---
+++ testsuite/synth/issue1860/testsuite.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+. ../../testenv.sh
+
+GHDL_STD_FLAGS=--std=08
+synth test.vhdl test.psl test_sub.vhdl -e test > syn_test.vhdl
+
+echo "Test successful"
