c6a66177de52ea5dc83582c24151e8ef7e86a266
Tristan Gingold <tgingold@free.fr>
2021-10-01 19:00:50 +0200

    testsuite/gna: add a test for previous commit

---
+++ testsuite/gna/bug0120/print4.vhdl
@@ -0,0 +1,7 @@
+entity ent1 is
+end entity;
+
+architecture rtl of ent1 is
+ signal bwe : bit_vector (3 downto 0);
+begin
+end architecture;
--- testsuite/gna/bug0120/testsuite.sh
+++ testsuite/gna/bug0120/testsuite.sh
@@ -8,4 +8,7 @@ diff_nocr print1.ref print1.out
 $GHDL fmt --std=08 --range=10:10 print1.vhdl > print2.out
 diff_nocr print2.ref print2.out
 
+$GHDL fmt --range=5:5 print4.vhdl > print4.out
+diff_nocr print4.ref print4.out
+
 echo "Test successful"
