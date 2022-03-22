7028d03e510297f383586e1da099205294f29b95
Tristan Gingold <tgingold@free.fr>
2022-03-22 21:38:07 +0100

    testsuite/gna/issue2005: handle --disable-libghdl

--- testsuite/gna/issue2005/testsuite.sh
+++ testsuite/gna/issue2005/testsuite.sh
@@ -6,9 +6,13 @@ set -x
 
 if c_compiler_is_available; then
 	lib=$("$GHDL" --libghdl-name)
+	lib_path="$("$GHDL" --libghdl-library-path)"
+	if [ ! -f "$lib_path" ]; then
+	    echo "no libghdl"
+	    exit 0
+	fi
 	lib=${lib#lib} # strip "lib" prefix
 	lib=${lib%.*} # strip .so/.dll suffix
-	lib_path="$("$GHDL" --libghdl-library-path)"
 
 	libdir="$(dirname "$lib_path")"
 	incdir="$("$GHDL" --libghdl-include-dir)"
