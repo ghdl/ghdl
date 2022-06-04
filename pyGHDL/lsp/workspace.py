b0186f723301f9d7b82b831ed7690020d58768e6
Tristan Gingold <tgingold@free.fr>
2022-06-04 09:32:02 +0200

    pyGHDL/lsp: enable warning for unassociated ports

--- pyGHDL/lsp/workspace.py
+++ pyGHDL/lsp/workspace.py
@@ -54,6 +54,7 @@ class Workspace(object):
         # Do not consider analysis order issues.
         flags.Flag_Elaborate_With_Outdated.value = True
         libghdl.errorout.Enable_Warning(errorout.Msgid.Warnid_Unused, True)
+        libghdl.errorout.Enable_Warning(errorout.Msgid.Warnid_No_Assoc, True)
         self.read_project()
         self.set_options_from_project()
         if libghdl.analyze_init_status() != 0:
