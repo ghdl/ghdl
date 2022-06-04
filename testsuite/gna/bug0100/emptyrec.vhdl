7b03815747527c625c6c913bf019307e4495ad7e
Tristan Gingold <tgingold@free.fr>
2022-06-04 09:18:06 +0200

    testsuite/gna: add a test for previous commit

---
+++ testsuite/gna/bug0100/emptyrec.vhdl
@@ -0,0 +1,11 @@
+entity emptyrec is
+  port (
+    clk_i           : in  bit
+  );
+end emptyrec;
+
+architecture arch of emptyrec is
+  type t_counter_config is record
+  end record;
+begin
+end arch;
--- testsuite/gna/bug0100/testsuite.sh
+++ testsuite/gna/bug0100/testsuite.sh
@@ -33,6 +33,7 @@ analyze_failure --force-analysis name4.vhdl
 analyze_failure --force-analysis inst2.vhdl
 analyze_failure arr_err1.vhdl
 analyze_failure --force-analysis oper1.vhdl
+analyze_failure --force-analysis emptyrec.vhdl
 
 if analyze_failure --force-analysis notype1.vhdl 2>&1 | grep -q "indexed name"; then
   :
