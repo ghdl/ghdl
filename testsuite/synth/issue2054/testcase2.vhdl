e15629320e954921f1332bfe7c97e3fc18f52832
Tristan Gingold <tgingold@free.fr>
2022-06-15 04:57:20 +0200

    testsuite/synth: add a test for #2093

---
+++ testsuite/synth/issue2054/testcase2.vhdl
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity testcase is
+    port (
+        clk : in std_logic;
+        i   : in std_ulogic_vector(63 downto 0);
+        o   : out std_ulogic_vector(63 downto 0)
+        );
+end entity testcase;
+
+architecture behaviour of testcase is
+    signal edge : std_ulogic_vector(63 downto 0);
+begin
+    testcase_0: process(clk)
+    begin
+        if rising_edge(clk) then
+            edge <= i;
+            o <= edge;
+        end if;
+    end process;
+end behaviour;
--- testsuite/synth/issue2054/testsuite.sh
+++ testsuite/synth/issue2054/testsuite.sh
@@ -3,8 +3,17 @@
 . ../../testenv.sh
 
 synth --out=verilog flip_flop.vhdl -e > syn_flip_flop.v
-
 if grep "input  wire" syn_flip_flop.v; then
+  exit 1
+fi
+
+synth --out=verilog testcase2.vhdl -e > syn_testcase2.v
+if grep "assign edge" syn_testcase2.v; then
+  exit 1
+fi
+
+synth --out=verilog testcase3.vhdl -e > syn_testcase3.v
+if grep "edge =" syn_testcase3.v; then
   exit 1
 fi
 
