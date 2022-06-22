53fbe284740a23af5457fe12abd4715eb2309abd
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2022-06-22 09:03:55 +0200

    Exposed new flags from libghdl: mb_comment, fexplicit, frelaxed.
    (cherry picked from commit 2a78cc687299c5bc0f4f65127957dbe4d7176027)

--- pyGHDL/libghdl/flags.py
+++ pyGHDL/libghdl/flags.py
@@ -49,7 +49,10 @@ assert sizeof(c_bool) == 1
 
 Flag_Elocations = c_bool.in_dll(libghdl, "flags__flag_elocations")
 
-Verbose = c_bool.in_dll(libghdl, "flags__verbose")
+Verbose = c_bool.in_dll(libghdl, "flags__verbose")               #: Internal boolean flag representing :option:`-v`.
+MB_Comment = c_bool.in_dll(libghdl, "flags__mb_comment")         #: Internal boolean flag representing :option:`--mb-comments`.
+Explicit = c_bool.in_dll(libghdl, "flags__flag_explicit")        #: Internal boolean flag representing :option:`-fexplicit`.
+Relaxed = c_bool.in_dll(libghdl, "flags__flag_relaxed_rules")    #: Internal boolean flag representing :option:`-frelaxed`.
 
 Flag_Elaborate_With_Outdated = c_bool.in_dll(libghdl, "flags__flag_elaborate_with_outdated")
 
