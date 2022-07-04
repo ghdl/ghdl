c4f2d8305e6798a530f772887dee3592e872ac53
Tristan Gingold <tgingold@free.fr>
2022-07-04 21:11:56 +0200

    netlists-disp_verilog: handle Id_Abs.  Fix #2113

--- src/synth/netlists-disp_verilog.adb
+++ src/synth/netlists-disp_verilog.adb
@@ -854,7 +854,7 @@ package body Netlists.Disp_Verilog is
          when Id_Neg =>
             Disp_Template ("  assign \o0 = -\i0;" & NL, Inst);
          when Id_Abs=>
-            Disp_Template ("  \o0 <= std_logic_vector(abs \si0);" & NL, Inst);
+            Disp_Template ("  \o0 <= \si0 >= 0 ? \i0 : -\i0;" & NL, Inst);
          when Id_Extract =>
             Disp_Template ("  assign \o0 = ", Inst);
             Disp_Extract (Inst);
