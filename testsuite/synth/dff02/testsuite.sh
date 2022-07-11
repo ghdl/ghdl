328aa863c6258c89e74ad04895dfc0484fe859e0
Tristan Gingold <tgingold@free.fr>
2022-07-11 07:33:34 +0200

    testsuite/synth/dff02: do not run dff06 (creates a latch)

--- testsuite/synth/dff02/testsuite.sh
+++ testsuite/synth/dff02/testsuite.sh
@@ -2,7 +2,8 @@
 
 . ../../testenv.sh
 
-for t in dff05 dff06 dff08 dff08a dff08b dff08c dff08d dff09; do
+# dff06 creates a latch (that is unused)
+for t in dff05 dff08 dff08a dff08b dff08c dff08d dff09; do
     synth_tb $t
 done
 
