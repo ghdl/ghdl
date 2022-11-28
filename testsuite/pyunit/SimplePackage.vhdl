2161dcbb0077057e1da026368ef44e056e637d43
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2022-11-28 23:18:48 +0100

    Added comments to test files.

--- testsuite/pyunit/SimplePackage.vhdl
+++ testsuite/pyunit/SimplePackage.vhdl
@@ -2,12 +2,14 @@ library ieee;
 use     ieee.std_logic_1164.all;
 use     ieee.numeric_std.all;
 
+-- Documentation before pack_1
 package pack_1 is
+	-- Global constant const_1
 	constant const_1 : boolean := false;
 
 end package;
 
 package body pack_1 is
-	constant const_1 : boolean := true;
+	constant const_2 : boolean := true;
 
 end package body;
