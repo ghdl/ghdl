58fa04a4d96a304146ae7ebaf59da6cb10886105
Tristan Gingold <tgingold@free.fr>
2022-12-23 16:14:24 +0100

    testsuite/gna/bug0130: reindentation

--- testsuite/gna/bug0130/word_entity1.vhdl
+++ testsuite/gna/bug0130/word_entity1.vhdl
@@ -2,9 +2,9 @@
 entity word_entity is
    generic (
        package word40 is new work.word_generic_pkg
-       generic map (
-           WIDTH => 40  -- not a generic in word_generic_pkg
-       )
+           generic map (
+               WIDTH => 40  -- not a generic in word_generic_pkg
+           )
    );
 end entity;
 
