bad2f273cc5e35dd6ff2d1a5f4458291051d6b5f
Tristan Gingold <tgingold@free.fr>
2023-01-11 19:12:33 +0100

    testsuite/gna: adjust harness for #1125

--- testsuite/gna/issue1125/testsuite.sh
+++ testsuite/gna/issue1125/testsuite.sh
@@ -4,7 +4,7 @@
 
 export GHDL_STD_FLAGS=--std=08
 analyze etest.vhdl
-elab_simulate_failure etest > etest.err
+elab_simulate_failure etest > etest.err 2>&1
 grep "slice direction" etest.err
 
 clean
