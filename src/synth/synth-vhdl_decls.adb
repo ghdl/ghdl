bccafab9f706394f70cdf88dfbe12f711ead415c
Tristan Gingold <tgingold@free.fr>
2023-01-11 05:18:46 +0100

    synth: allow file declaration in protected objects

--- src/synth/synth-vhdl_decls.adb
+++ src/synth/synth-vhdl_decls.adb
@@ -420,7 +420,8 @@ package body Synth.Vhdl_Decls is
             when Iir_Kind_Type_Declaration
               | Iir_Kind_Anonymous_Type_Declaration
               | Iir_Kind_Subtype_Declaration
-              | Iir_Kind_Variable_Declaration =>
+              | Iir_Kind_Variable_Declaration
+              | Iir_Kind_File_Declaration =>
                Elab.Vhdl_Decls.Elab_Declaration
                  (Obj_Inst, Decl, True, Last_Type);
             when Iir_Kind_Function_Declaration
