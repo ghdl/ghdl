19836e1f0de07711e63c1fc8cd24ba0d123c584b
Tristan Gingold <tgingold@free.fr>
2023-01-21 08:22:48 +0100

    ghdldrv: extract trans_link from ghdlrun

---
+++ src/vhdl/translate/trans_link.ads
@@ -0,0 +1,19 @@
+--  Link imported routines in Trans_Decls with GRT.
+--  Copyright (C) 2002-2023 Tristan Gingold
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <gnu.org/licenses>.
+
+package Trans_Link is
+   procedure Link;
+end Trans_Link;
