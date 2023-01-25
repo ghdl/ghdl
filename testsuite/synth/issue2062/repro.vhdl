9443cb4d447460f592b645c6bc66f73bbee87c1e
Tristan Gingold <tgingold@free.fr>
2023-01-25 08:16:43 +0100

    testsuite/synth: adjust reproducer for #2062

--- testsuite/synth/issue2062/repro.vhdl
+++ testsuite/synth/issue2062/repro.vhdl
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity repro is port (
    a : in  std_logic_vector(5 downto 0);
-   y : out std_ulogic_vector(3 downto -2));
+   y : out std_ulogic_vector(7 downto 2));
 end entity;
 
 architecture beh of repro is
