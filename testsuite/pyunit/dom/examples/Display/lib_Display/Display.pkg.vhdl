73e4dfd71756198d0d7ab65b1894b21ee9f13c79
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2023-05-14 20:16:20 +0200

    Updated example files for DOM testing.
    (cherry picked from commit 8853d4b3ddd84ed06e33c4902b24e6784915709e)

--- testsuite/pyunit/dom/examples/Display/lib_Display/Display.pkg.vhdl
+++ testsuite/pyunit/dom/examples/Display/lib_Display/Display.pkg.vhdl
@@ -8,16 +8,9 @@ use     IEEE.std_logic_1164.all;
 use     IEEE.numeric_std.all;
 
 -- Package with stop watch specific types.
-package StopWatch_pkg is
+package Display_pkg is
 	subtype T_BCD is unsigned(3 downto 0);
 	type T_BCD_Vector is array(natural range <>) of T_BCD;
-
-	type T_DIGIT_CONFIGURATION is record
-		Modulo : positive;
-		Dot    : std_logic;
-	end record;
-
-	type T_STOPWATCH_CONFIGURATION is array(natural range <>) of T_DIGIT_CONFIGURATION;
 
 	-- Encoder that translates from 4-bit binary (BCD) to 7-segment code.
 	--
