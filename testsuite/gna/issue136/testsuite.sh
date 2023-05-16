f5bbe7f42bd79850fb8c6742ebc2cfed198619b7
Tristan Gingold <tgingold@free.fr>
2023-05-16 21:29:13 +0200

    testsuite/gna: add a test for non-existing response file

--- testsuite/gna/issue136/testsuite.sh
+++ testsuite/gna/issue136/testsuite.sh
@@ -2,6 +2,16 @@
 
 . ../../testenv.sh
 
+if $GHDL @unknown.rsp 2> nexist.err; then
+  echo "failure expected"
+  exit 1
+fi
+if grep -q Bug nexist.err; then
+  echo "Bug box not expected"
+  exit 1
+fi
+rm -f nexist.err
+
 $GHDL @args.resp
 
 rm -rf html
