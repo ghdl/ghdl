2dbb2e23c2e5bdc19c2bc41e8ffe69abc8e51dac
Tristan Gingold <tgingold@free.fr>
2023-06-17 08:23:31 +0200

    testsuite/synth: add a test for verilog translate on/off

---
+++ testsuite/synth/transoff01/translate_off1.v
@@ -0,0 +1,8 @@
+module toff1 (output [7:0] data);
+
+  // synthesis translate off
+   reg 	      dbg;
+   // synthesis translate on
+
+   assign data = 8'h24;
+endmodule
---
+++ testsuite/synth/transoff01/translate_off2.v
@@ -0,0 +1,10 @@
+module toff2 (output [7:0] data);
+
+   assign data = 8'ha6
+
+  // synthesis translate off
+		 + 1
+  // synthesis translate on
+		 ;
+   
+endmodule
