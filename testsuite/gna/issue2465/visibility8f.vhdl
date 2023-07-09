dc4b9f4d10c65fc5d3ba29bc2415e51af0ea990b
Tristan Gingold <tgingold@free.fr>
2023-07-09 18:22:50 +0200

    testsuite/gna: add tests for #2465

---
+++ testsuite/gna/issue2465/visibility8f.vhdl
@@ -0,0 +1,20 @@
+entity visibility8 is
+end entity;
+
+architecture test of visibility8 is
+    constant c1 : integer := 1;
+    subtype t1 is integer;
+    type t2 is range 1 to 3;
+    signal s1 : bit_vector(1 to 6);
+    shared variable v1 : integer;
+    alias a1 is c1;
+    constant f1 : file_open_kind := READ_MODE;
+    type ft is file of character;
+begin
+    p1: process is
+        alias a1 is a1;                 -- Error
+    begin
+        wait;
+    end process;
+
+end architecture;
