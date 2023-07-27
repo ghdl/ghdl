519f072a1dd7d036c75b2e135c94dc4a2f702e7b
Hipolito Guzman <hguzman@us.es>
2023-07-27 13:39:19 +0200

    test for issue1425: add a couple of tests for records

--- testsuite/gna/issue1425/testsuite.sh
+++ testsuite/gna/issue1425/testsuite.sh
@@ -2,12 +2,21 @@
 
 . ../../testenv.sh
 
+# Test 1: counter.vhdl (we expect some warnings)
+#
 # We should get warnings about an incomplete and overspecified sensitivity
 # list, so let's direct stderr to a file
 analyze counter.vhdl 2> analyze.log
-
 # Compare if our warnings match the expected warnings
 diff_nocr analyze.log expected.txt
+
+# Test 2: records.vhdl (we expect no warnings, so fail if we get any)
+GHDL_STD_FLAGS=-Werror
+analyze records.vhdl
+
+# Test 3: records_in_records.vhdl (also we expect no warnings)
+GHDL_STD_FLAGS=-Werror
+analyze records_in_records.vhdl
 
 clean
 
