9990e6eb652afa60f0d89112f38b353478c2a83e
Tristan Gingold <tgingold@free.fr>
2023-08-24 21:48:33 +0200

    vhdl-sem_stmts: do not add drivers for force/release
    Fix #2471

--- src/vhdl/vhdl-sem_stmts.adb
+++ src/vhdl/vhdl-sem_stmts.adb
@@ -470,11 +470,20 @@ package body Vhdl.Sem_Stmts is
       Target_Prefix : Iir;
       Guarded_Target : Tri_State_Type;
       Targ_Obj_Kind : Iir_Kind;
+      Add_Driver : Boolean;
    begin
       Target_Object := Check_Simple_Signal_Target_Object (Target);
       if Target_Object = Null_Iir then
          return;
       end if;
+
+      case Get_Kind (Stmt) is
+         when Iir_Kind_Signal_Force_Assignment_Statement
+           | Iir_Kind_Signal_Release_Assignment_Statement =>
+            Add_Driver := False;
+         when others =>
+            Add_Driver := True;
+      end case;
 
       Target_Prefix := Get_Object_Prefix (Target_Object);
       Targ_Obj_Kind := Get_Kind (Target_Prefix);
@@ -484,16 +493,24 @@ package body Vhdl.Sem_Stmts is
                Error_Msg_Sem
                  (+Target, "%n can't be assigned", +Target_Prefix);
             else
-               Sem_Add_Driver (Target_Object, Stmt);
+               if Add_Driver then
+                  Sem_Add_Driver (Target_Object, Stmt);
+               end if;
             end if;
          when Iir_Kind_Interface_View_Declaration =>
             Check_View_Signal_Target (Target);
-            Sem_Add_Driver (Target_Object, Stmt);
+            if Add_Driver then
+               Sem_Add_Driver (Target_Object, Stmt);
+            end if;
          when Iir_Kind_Signal_Declaration =>
-            Sem_Add_Driver (Target_Object, Stmt);
+            if Add_Driver then
+               Sem_Add_Driver (Target_Object, Stmt);
+            end if;
             Set_Use_Flag (Target_Prefix, True);
          when Iir_Kind_External_Signal_Name =>
-            Sem_Add_Driver (Target_Object, Stmt);
+            if Add_Driver then
+               Sem_Add_Driver (Target_Object, Stmt);
+            end if;
          when Iir_Kind_Guard_Signal_Declaration =>
             Error_Msg_Sem (+Stmt, "implicit GUARD signal cannot be assigned");
             return;
