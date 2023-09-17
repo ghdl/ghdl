7109d16464258336e081e9ea34c56ba428e71a44
Tristan Gingold <tgingold@free.fr>
2023-09-17 19:34:18 +0200

    trans-chap7: handle external names in expressions.
    Fix #548

--- src/vhdl/translate/trans-chap7.adb
+++ src/vhdl/translate/trans-chap7.adb
@@ -5071,7 +5071,8 @@ package body Trans.Chap7 is
             | Iir_Kind_Transaction_Attribute
             | Iir_Kind_Guard_Signal_Declaration
             | Iir_Kind_Attribute_Value
-            | Iir_Kind_Attribute_Name =>
+            | Iir_Kind_Attribute_Name
+            | Iir_Kinds_External_Name =>
             Res := M2E (Chap6.Translate_Name (Expr, Mode_Value));
 
          when Iir_Kind_Iterator_Declaration =>
