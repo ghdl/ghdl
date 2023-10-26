a9ef160b11aafb74c6d1c7993007d16ebe2263dc
Tristan Gingold <tgingold@free.fr>
2023-10-26 07:29:40 +0200

    testsuite/gna/fst01: remove ref files
    fst output is not deterministic

--- testsuite/gna/fst01/testsuite.sh
+++ testsuite/gna/fst01/testsuite.sh
@@ -8,14 +8,16 @@ if ! ghdl_is_preelaboration; then
     exit 0
 fi
 
-files="hier1 hier2
+files="hier1 hier2 type1 enum1 enum2 forgen1 casegen1
  ifgen1 compinst1 compinst2"
-# "type1 enum1 enum2 forgen1 casegen1"
 
 for f in $files; do
     analyze ${f}.vhdl
     elab_simulate $f --fst=${f}.fst --fst-nodate
-    cmp ${f}.fst ${f}.ref
+    if [ -f refs/${f}.fst ]; then
+	# Test only if available (fst files are not very deterministic)
+	cmp ${f}.fst refs/${f}.fst
+    fi
 done
 
 clean
