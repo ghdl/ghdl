c1bc9c06ba24f64c6d908716f274ef9341f44772
Tristan Gingold <tgingold@free.fr>
2023-11-28 19:07:45 +0100

    synth_pathname: fix iterator value evaluation.
    Fix #2546

--- src/synth/elab-vhdl_expr.adb
+++ src/synth/elab-vhdl_expr.adb
@@ -384,8 +384,8 @@ package body Elab.Vhdl_Expr is
 
                --  [...] and the value of the expression shall belong to the
                --  discrete range specified for the generate parameter.
-               Param_Rng := Get_Subtype_Object (Loc_Inst, Get_Type (Param));
-               Idx := Synth_Expression_With_Type (Loc_Inst, Expr, Param_Rng);
+               Param_Rng := Get_Subtype_Object (Cur_Inst, Get_Type (Param));
+               Idx := Synth_Expression_With_Type (Cur_Inst, Expr, Param_Rng);
                if Idx = No_Valtyp then
                   return No_Valtyp;
                end if;
