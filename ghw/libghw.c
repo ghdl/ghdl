422e85fb2f826926d643a9c1b4a2a5efa8484cf4
Tristan Gingold <tgingold@free.fr>
2024-02-21 19:52:37 +0100

    Use lgpl for libghw
    For #1812

--- ghw/libghw.c
+++ ghw/libghw.c
@@ -1,18 +1,18 @@
 /* GHDL Wavefile reader library.
   Copyright (C) 2005 Tristan Gingold
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 2 of the License, or
-  (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
+  This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <gnu.org/licenses>.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; If not, see <gnu.org/licenses>.
 */
 
 #include <assert.h>
