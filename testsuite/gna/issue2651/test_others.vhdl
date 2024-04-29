b4e4eef5ff2f267204903fce3cc682e84243dd2d
Tristan Gingold <tgingold@free.fr>
2024-04-29 09:06:04 +0200

    testsuite/gna: add a test for #2651

---
+++ testsuite/gna/issue2651/test_others.vhdl
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity test_others is
+    port (
+        data_i : in std_ulogic_vector;
+        data_o : out std_ulogic_vector
+    );
+end;
+
+architecture arch of test_others is
+    constant WIDTH_IN : natural := data_i'LENGTH;
+    constant WIDTH_OUT : natural := data_o'LENGTH;
+    function extend(data : std_ulogic_vector) return std_ulogic_vector
+    is
+        variable result : data_o'SUBTYPE;
+--         variable result : std_ulogic_vector(WIDTH_OUT-1 downto 0);
+    begin
+        result(WIDTH_IN-1 downto 0) := data;
+        result(WIDTH_OUT-1 downto WIDTH_IN) := (others => '0');
+        return result;
+    end;
+begin
+--     data_o <= (WIDTH_IN-1 downto 0 => data_i, others => '0');
+    data_o <= extend(data_i);
+end;
