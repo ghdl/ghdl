c5acd65195b5ce79201038c42b171f60db2aca9b
Tristan Gingold <tgingold@free.fr>
2024-06-09 18:25:43 +0200

    testsuite/gna: add a test for #2669

---
+++ testsuite/gna/issue2669/foo.vhdl
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity foo is
+  port(
+    clk : in std_logic;
+    reset : in std_logic
+  );
+end foo;
+
+architecture arch of foo is
+  signal a : std_logic;
+begin
+  assert always (a = '1')       abort (reset = '1') @rising_edge(clk); -- ok
+  assert always (a = '1')  sync_abort (reset = '1') @rising_edge(clk); -- ok
+--  assert always (a = '1') async_abort (reset = '1') @rising_edge(clk); -- error
+  assert (always a = '1') async_abort (reset = '1') @rising_edge(clk); -- error
+end arch;
+
