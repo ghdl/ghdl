ded5de52727f05c22c416dbd4396022bab18d6bc
Tristan Gingold <tgingold@free.fr>
2024-07-19 07:29:03 +0200

    testsuite/gna: add a test for #2690

--- testsuite/gna/issue2690/testsuite.sh
+++ testsuite/gna/issue2690/testsuite.sh
@@ -4,7 +4,7 @@
 
 if ghdl_is_preelaboration; then
     export GHDL_STD_FLAGS=--std=08
-    for f in bug_tb bug_tb3 bug_tb4; do
+    for f in bug_tb bug_tb3 bug_tb4 bug_tb5; do
 	analyze $f.vhdl
 	elab_simulate ghdl_bug_tb
 	clean
