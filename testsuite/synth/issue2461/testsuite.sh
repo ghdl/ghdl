ec315f0a6be497c70aaf7a06b7d3d9461d372b27
Tristan Gingold <tgingold@free.fr>
2024-08-14 19:31:18 +0200

    testsuite/synth: add a test for previous commit

--- testsuite/synth/issue2461/testsuite.sh
+++ testsuite/synth/issue2461/testsuite.sh
@@ -5,6 +5,7 @@
 GHDL_STD_FLAGS="--std=08 --latches"
 
 synth_only repro3
+synth_only repro4
 
 synth_failure knn.vhdl argmin.vhdl distancias.vhdl insere.vhdl moda.vhdl norm.vhdl pacote_aux.vhdl top.vhdl -e > syn_top.vhdl
 
