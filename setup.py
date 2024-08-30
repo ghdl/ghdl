c71b5da270d0cdd18b0df053293887669b86e32b
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2024-08-30 21:43:49 +0200

    Create platform specific wheels.

--- setup.py
+++ setup.py
@@ -33,16 +33,40 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ============================================================================
 #
-from pathlib             import Path
-from setuptools          import setup
-from pyTooling.Licensing import GPL_2_0_or_later
-from pyTooling.Packaging import DescribePythonPackageHostedOnGitHub
+from pathlib                  import Path
+from setuptools               import setup, Distribution
+
+from pyTooling.Licensing      import GPL_2_0_or_later
+from pyTooling.Packaging      import DescribePythonPackageHostedOnGitHub
 
 gitHubNamespace = "ghdl"
 packageName =  "pyGHDL"
 packageDirectory = packageName
 packageInformationFile = Path(f"{packageDirectory}/__init__.py")
 requirementsFile = Path(f"{packageDirectory}/requirements.txt")
+
+
+# Package data files
+sourceDirectory = Path().cwd() / "pyGHDL"
+dataFiles = [sourceDirectory / "py.typed"]
+additional = {}
+
+# libGHDL shared object and pre-analyzed files
+libGHDLDirectory = sourceDirectory / "lib"
+if libGHDLDirectory.exists():
+    dataFiles.extend(libGHDLDirectory.rglob("lib*.dll"))
+    ghdlDirectory = libGHDLDirectory / "ghdl"
+    dataFiles.extend(ghdlDirectory.rglob("**/*.vhdl"))
+    dataFiles.extend(ghdlDirectory.rglob("**/*.cf"))
+
+    # Extend the Distribution class to force platform specific wheel
+    class BinaryDistribution(Distribution):
+        def has_ext_modules(self):
+            return True
+
+    additional["include_package_data"] = True
+    additional["distclass"] = BinaryDistribution
+
 
 setup(
     **DescribePythonPackageHostedOnGitHub(
@@ -55,7 +79,7 @@ setup(
         developmentStatus="beta",
         classifiers=[
             "Operating System :: MacOS",
-            "Operating System :: Microsoft :: Windows :: Windows 10",
+            "Operating System :: Microsoft :: Windows :: Windows 11",
             "Operating System :: POSIX :: Linux",
             "Intended Audience :: Developers",
             "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
@@ -65,11 +89,12 @@ setup(
             "Topic :: Utilities",
         ],
         consoleScripts={
-            "ghdl-ls": "pyGHDL.cli.lsp:main",
+            "ghdl-ls":  "pyGHDL.cli.lsp:main",
             "ghdl-dom": "pyGHDL.cli.dom:main"
         },
         dataFiles={
-            packageName: ["py.typed"]
+            packageName: [str(file.relative_to(sourceDirectory)) for file in dataFiles]
         }
-    )
+    ),
+    **additional,
 )
