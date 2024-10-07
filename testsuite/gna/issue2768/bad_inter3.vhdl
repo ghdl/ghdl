04d017da05534ce789010faa8889797ce5401d97
Tristan Gingold <tgingold@free.fr>
2024-10-07 19:33:09 +0200

    testsuite/gna: add a test for #2768

---
+++ testsuite/gna/issue2768/bad_inter3.vhdl
@@ -0,0 +1,4 @@
+entity e is
+  generic(type t);
+  signal w:integer range 0 to t(0);
+end;
--- testsuite/gna/issue2768/testsuite.sh
+++ testsuite/gna/issue2768/testsuite.sh
@@ -4,6 +4,7 @@
 
 analyze_failure --std=08 --force-analysis bad_inter.vhdl
 analyze_failure --std=08 --force-analysis bad_inter2.vhdl
+analyze_failure --std=08 --force-analysis bad_inter3.vhdl
 analyze_failure --std=08 --force-analysis many_parent.vhdl
 analyze_failure --std=08 --force-analysis missing_id.vhdl
 
