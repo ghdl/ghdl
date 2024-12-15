d2a566cb58347c4cb4323439cd0faec039d9d42b
Tristan Gingold <tgingold@free.fr>
2024-12-15 19:16:19 +0100

    testsuite/gna: enable #2005 on all platforms

--- testsuite/gna/issue2005/testsuite.sh
+++ testsuite/gna/issue2005/testsuite.sh
@@ -17,13 +17,9 @@ if c_compiler_is_available; then
 	libdir="$(dirname "$lib_path")"
 	incdir="$("$GHDL" --libghdl-include-dir)"
 
-	if [ `uname -s` = Darwin ]; then # FIXME: -a `uname -m` = arm64 ]; then
-	    echo "XFAIL on macos arm64"
-	else
-	    $CC -I"$incdir" test.c "$("$GHDL" --libghdl-library-path)"
-	    $CC -I"$incdir" -L"$libdir" test.c -l"$lib"
-	    rm -f a.out
-	fi
+	$CC -I"$incdir" test.c "$("$GHDL" --libghdl-library-path)"
+	$CC -I"$incdir" -L"$libdir" test.c -l"$lib"
+	rm -f a.out
 fi
 
 echo "Test successful"
