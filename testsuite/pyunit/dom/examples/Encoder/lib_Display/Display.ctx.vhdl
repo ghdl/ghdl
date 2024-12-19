6d5a7b553601175a33dd43f5244883d9dd3a744f
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2024-12-19 11:15:21 +0100

    Reorganized example code.

---
+++ testsuite/pyunit/dom/examples/Encoder/lib_Display/Display.ctx.vhdl
@@ -0,0 +1,11 @@
+-- Author:  Patrick Lehmann
+-- License: MIT
+--
+-- undocumented
+--
+context Display_ctx is
+	library lib_Utilities;
+	context lib_Utilities.Utilities_ctx;
+
+	use work.Display_pkg.all;
+end context;
