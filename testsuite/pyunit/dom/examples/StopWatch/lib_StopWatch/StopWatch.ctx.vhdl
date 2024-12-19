efaba71ae151148521319e906aef7785bfa7ac93
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2024-12-19 11:15:21 +0100

    Reorganized example code.

--- testsuite/pyunit/dom/examples/StopWatch/lib_StopWatch/StopWatch.ctx.vhdl
+++ testsuite/pyunit/dom/examples/StopWatch/lib_StopWatch/StopWatch.ctx.vhdl
@@ -7,5 +7,8 @@ context StopWatch_ctx is
 	library lib_Utilities;
 	context lib_Utilities.Utilities_ctx;
 
+	library lib_Display;
+	use     lib_Display.Display_pkg.all;
+
 	use work.StopWatch_pkg.all;
 end context;
