3a9aaec2a92be9eb11b106a25c2d22e4eb1c0080
Tristan Gingold <tgingold@free.fr>
2025-01-31 07:52:14 +0100

    testsuite/gna: add a test for #2871

---
+++ testsuite/gna/issue2871/example.vhdl
@@ -0,0 +1,11 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity example is
+  generic (
+    PARAMETER : std_logic_vector(7 downto 0));
+end example;
+
+architecture behavioral of example is
+begin
+end behavioral;
