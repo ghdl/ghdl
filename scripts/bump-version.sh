1bb903b4477f5f3018c49712c29ed94e5e64bf64
Tristan Gingold <tgingold@free.fr>
2025-02-28 07:33:55 +0100

    Add scripts/bump-version.sh

---
+++ scripts/bump-version.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+if [ $0 != "./scripts/bump-version.sh" ]; then
+    echo "$0 must be called from ghdl base directory"
+    exit 2
+fi
+
+if [ $# -ne 1 ]; then
+    echo "usage: $0 VERSION"
+    exit 2
+fi
+
+ver=$1
+
+# Version should be:
+case "$ver"@ in
+    [0-9].[0-9].[0-9]@) echo "Release version $ver";;
+    [0-9].[0-9].[0-9]-dev@) echo "Development branch $ver";;
+    [0-9].[0-9].[0-9]-rc[0-9]@) echo "Release candidate $ver";;
+    *) echo "Incorrect version name ($ver)"; exit 3;;
+esac
+
+set -e
+
+sed -i "s/^ghdl_version=.*/ghdl_version=\"$ver\"/" configure
+sed -i "s/^__version__ =.*/__version__ = \"$ver\"/" pyGHDL/__init__.py
+
+# Hyphen are not allowed in pkgver
+pkgver=$(echo $ver | sed 's/-/_/')
+
+for f in dist/msys2/*/PKGBUILD; do
+    sed -i "s/^pkgver=.*/pkgver=$pkgver/" $f
+done
+
+echo "Done"
+exit 0
