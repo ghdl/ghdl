b97d008e263089553626268c94cc650995a85d7f
Tristan Gingold <tgingold@free.fr>
2025-05-02 08:32:22 +0200

    find_generate_body_id: handle unused generate block
    Fix #2982

--- src/simul/simul-vhdl_compile.adb
+++ src/simul/simul-vhdl_compile.adb
@@ -1493,6 +1493,7 @@ package body Simul.Vhdl_Compile is
    is
       Res : Uns32;
       N : Node;
+      B : Node;
    begin
       pragma Assert (Get_Kind (Bod) = Iir_Kind_Generate_Statement_Body);
       Res := 0;
@@ -1500,10 +1501,13 @@ package body Simul.Vhdl_Compile is
          when Iir_Kind_If_Generate_Statement =>
             N := Stmt;
             while N /= Null_Node loop
-               if Get_Generate_Statement_Body (N) = Bod then
+               B := Get_Generate_Statement_Body (N);
+               if B = Bod then
                   return Res;
                end if;
-               Res := Res + 1;
+               if Get_Use_Flag (B) then
+                  Res := Res + 1;
+               end if;
                N := Get_Generate_Else_Clause (N);
             end loop;
             raise Internal_Error;
