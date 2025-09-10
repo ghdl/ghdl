e9cfc54c8ea506d3be7c577e502d0cf01783f4cb
Tristan Gingold <tgingold@free.fr>
2025-09-10 20:56:28 +0200

    testsuite/synth: add a vhdl test for #2755

--- testsuite/synth/issue2755/testsuite.sh
+++ testsuite/synth/issue2755/testsuite.sh
@@ -5,4 +5,7 @@
 synth --out=verilog mul_block.vhdl -e > syn_mul_block.v
 grep -F '"WANNABE_STRING"' syn_mul_block.v
 
+synth --out=vhdl mul_block.vhdl -e > syn_mul_block.vhdl
+grep -F '"WANNABE_STRING"' syn_mul_block.vhdl
+
 echo "Test successful"
