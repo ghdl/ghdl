78ec7791ee7e0e45b95c69dacbdbedac806e642e
Tristan Gingold <tgingold@free.fr>
2025-11-15 13:57:26 +0100

    testsuite/synth: add a test for name 'do' in verilog output
    For #3094

--- testsuite/synth/issue3094/testsuite.sh
+++ testsuite/synth/issue3094/testsuite.sh
@@ -5,5 +5,10 @@
 synth --work=lib_a entityA.vhdl --work=lib_b entityB.vhdl --work=work wrapperA.vhdl wrapperB.vhdl top.vhdl -e > syn_top.vhdl
 analyze syn_top.vhdl
 
+synth --out=verilog entityA.vhdl -e > syn_entitya.v
+if fgrep -q ' do' syn_entitya.v ; then
+    exit 1
+fi
+
 echo "Test successful"
 
