0e50541b6f17744c61b172d9aec818fd360c1ac9
Tristan Gingold <tgingold@free.fr>
2025-11-19 18:35:43 +0100

    testsuite/gna: add tests for #3103

---
+++ testsuite/gna/issue3103/testsuite.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+. ../../testenv.sh
+
+if ghdl_is_preelaboration; then
+  export GHDL_STD_FLAGS=--std=08
+  analyze a.vhdl
+  elab_simulate tb_ghdl
+
+  clean
+
+  analyze a2.vhdl
+  elab_simulate tb_ghdl
+  
+  clean
+fi
+
+echo "Test successful"
