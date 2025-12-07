040c44f34c7070bd62bb70a57689d43dc730ddc9
Tristan Gingold <tgingold@free.fr>
2025-12-07 13:48:24 +0100

    testsuite/gna: always run #3110

--- testsuite/gna/issue3110/testsuite.sh
+++ testsuite/gna/issue3110/testsuite.sh
@@ -2,15 +2,13 @@
 
 . ../../testenv.sh
 
-if ghdl_is_preelaboration; then
-  export GHDL_STD_FLAGS=--std=08
-  analyze issue2.vhdl
-  elab_simulate issue2
+export GHDL_STD_FLAGS=--std=08
+analyze issue2.vhdl
+elab_simulate issue2
 
-  analyze issue.vhdl
-  elab_simulate issue
+analyze issue.vhdl
+elab_simulate issue
 
-  clean
-fi
+clean
 
 echo "Test successful"
