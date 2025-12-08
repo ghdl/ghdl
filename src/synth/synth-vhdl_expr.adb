fdf345076a40c8cd05dad5d38528c34aa6851407
Tristan Gingold <tgingold@free.fr>
2025-12-08 21:25:23 +0100

    synth-vhdl_expr: handle external constant name

--- src/synth/synth-vhdl_expr.adb
+++ src/synth/synth-vhdl_expr.adb
@@ -2311,6 +2311,7 @@ package body Synth.Vhdl_Expr is
             | Iir_Kinds_Object_Declaration
             | Iir_Kind_Non_Object_Alias_Declaration   -- For PSL
             | Iir_Kind_External_Signal_Name
+            | Iir_Kind_External_Constant_Name
             | Iir_Kind_Implicit_Dereference
             | Iir_Kind_Dereference
             | Iir_Kind_Above_Attribute
