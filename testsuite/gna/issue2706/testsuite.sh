adb6673a4acedf5bac2b9f7cf9654f60d7106bd0
Tristan Gingold <tgingold@free.fr>
2025-12-10 21:23:05 +0100

    testsuite/gna: check vcd feature for #2706

--- testsuite/gna/issue2706/testsuite.sh
+++ testsuite/gna/issue2706/testsuite.sh
@@ -2,16 +2,20 @@
 
 . ../../testenv.sh
 
+
 # Normal end
 analyze tb1.vhdl
-elab_simulate tb1 --vcd=tb1.vcd
-grep "#20000000" tb1.vcd
+elab tb1
+if ghdl_has_feature tb1 vcd; then
+  elab_simulate tb1 --vcd=tb1.vcd
+  grep "#20000000" tb1.vcd
 
-# assertion failure
-analyze tb3.vhdl
-elab_simulate_failure tb3 --vcd=tb3.vcd
-grep "#25000000" tb3.vcd
+  # assertion failure
+  analyze tb3.vhdl
+  elab_simulate_failure tb3 --vcd=tb3.vcd
+  grep "#25000000" tb3.vcd
 
-clean
+  clean
+fi
 
 echo "Test successful"
