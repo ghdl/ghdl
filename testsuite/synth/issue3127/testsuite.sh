e35c384519d2bfd31e1910c47a24df625de7ebcd
Tristan Gingold <tgingold@free.fr>
2025-12-12 08:15:48 +0100

    testsuite/synth: add a test for previous commit

--- testsuite/synth/issue3127/testsuite.sh
+++ testsuite/synth/issue3127/testsuite.sh
@@ -5,8 +5,12 @@
 export GHDL_STD_FLAGS=--std=08
 
 analyze --work=gaisler pkg.vhd
-analyze --work=gaisler entityA.vhd
 analyze top.vhd
+
+# Component of an external name is not bound
+synth_failure --keep-hierarchy=no top -e
+
+analyze --work=gaisler entityA.vhd
 if ghdl_is_preelaboration; then
   elab_simulate top
 fi
