54d0c17bbb3ed5acf20ef6a3e70492c3bac8c014
Tristan Gingold <tgingold@free.fr>
2025-12-16 19:08:20 +0100

    testsuite/synth: add a test to sns02

--- testsuite/synth/sns02/testsuite.sh
+++ testsuite/synth/sns02/testsuite.sh
@@ -6,7 +6,7 @@ GHDL_STD_FLAGS=-fsynopsys
 
 for t in conv01 conv02 conv03 \
 	 id01 neg01 abs01 \
-	 cmp01 cmp02 cmp03 cmp04 cmp05 cmp06 cmp07 \
+	 cmp01 cmp02 cmp03 cmp04 cmp05 cmp06 cmp07 cmp08 \
 	 add01 sub01 mul01 \
 	 ext01 shift01 reduce01
 do
