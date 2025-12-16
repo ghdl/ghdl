3d7f2188b29c7115fc64b165e8afe1f9d4d7d4d2
Tristan Gingold <tgingold@free.fr>
2025-12-16 06:15:56 +0100

    testsuite/synth: add a test for verilog output

--- testsuite/synth/cli01/testsuite.sh
+++ testsuite/synth/cli01/testsuite.sh
@@ -3,6 +3,7 @@
 . ../../testenv.sh
 
 synth -dm -de dpram1.vhdl -e > syn_dpram1.vhdl
+synth --out=verilog -dm -de dpram1.vhdl -e > syn_dpram1.v
 synth --stats -o=syn_dpram1.vhdl dpram1.vhdl -e
 
 echo "Test successful"
