c102351d516b86a33f31d73432ef091614efc578
Tristan Gingold <tgingold@free.fr>
2026-01-03 10:43:25 +0100

    testsuite/synth: add tests for addidx output

--- testsuite/synth/issue1220/testsuite.sh
+++ testsuite/synth/issue1220/testsuite.sh
@@ -4,4 +4,8 @@
 
 synth_tb top
 
+synth --out=verilog top.vhdl -e > syn_top.v
+synth --out=verilog -dm -de top.vhdl -e > syn_top_dm.v
+synth -dm -de top.vhdl -e > syn_top_dm.vhdl
+
 echo "Test successful"
