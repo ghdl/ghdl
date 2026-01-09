dd22b78ea9d73ff43403ed9fef1eeed725d0ce87
Tristan Gingold <tgingold@free.fr>
2026-01-09 08:01:50 +0100

    testsuite/synth: add a test in blackbox02

--- testsuite/synth/blackbox02/testsuite.sh
+++ testsuite/synth/blackbox02/testsuite.sh
@@ -2,6 +2,12 @@
 
 . ../../testenv.sh
 
-verilog_synth_tb my_or3 my_or.v
+# Using syn_black_box attribute
+verilog_synth_tb my_or3 my_or_bb.v
 
+# Using -lib/-no-lib
+synth my_or3.v -lib my_or.v -no-lib -e > syn_my_or3.v
+if fgrep -q " of my_or is" syn_my_or3.v; then
+    exit 1;
+fi
 echo "Test successful"
