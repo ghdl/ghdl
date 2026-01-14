6fcf8dd2ae6c4692475be9a5a4a2622909ddf78c
Tristan Gingold <tgingold@free.fr>
2026-01-14 07:53:32 +0100

    testsuite/gna: adjust harness of #3146

--- testsuite/gna/issue3146/testsuite.sh
+++ testsuite/gna/issue3146/testsuite.sh
@@ -2,10 +2,12 @@
 
 . ../../testenv.sh
 
-export GHDL_STD_FLAGS=--std=08
-analyze test.vhdl test.psl
-$GHDL -m $GHDL_STD_FLAGS test
+if ghdl_is_preelaboration; then
+    export GHDL_STD_FLAGS=--std=08
+    analyze test.vhdl test.psl
+    $GHDL -m $GHDL_STD_FLAGS test
 
-clean
+    clean
+fi
 
 echo "Test successful"
