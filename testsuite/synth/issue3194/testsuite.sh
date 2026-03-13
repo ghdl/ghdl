b1508f089de68b1d42a5e93d200d5d8ed4262a68
Tristan Gingold <tgingold@free.fr>
2026-03-13 07:56:46 +0100

    testsuite/synth: add a test for #3194

--- testsuite/synth/issue3194/testsuite.sh
+++ testsuite/synth/issue3194/testsuite.sh
@@ -8,5 +8,9 @@ for t in my_is_x; do
 done
 
 synth float16_pkg.vhdl fadd.vhdl -e > syn_fadd.vhdl
+analyze float16_pkg.vhdl syn_fadd.vhdl tb.vhdl
+elab_simulate tb --ieee-asserts=disable-at-0
+
+clean
 
 echo "Test successful"
