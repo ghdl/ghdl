1352f676a1b2e668c3d4ac55f991e68a8a854fb0
Tristan Gingold <tgingold@free.fr>
2026-03-15 17:25:07 +0100

    testsuite/synth: add a test for #3200

--- testsuite/synth/issue3200/testsuite.sh
+++ testsuite/synth/issue3200/testsuite.sh
@@ -4,6 +4,10 @@
 
 synth --out=verilog test.vhdl -e > syn_test.v
 
+# Attribute on architecture
 fgrep -q '(* testarch="hello 2" *) module test' syn_test.v
+
+# Attribute on component instantiation
+fgrep -q '(* testlabel="hello world" *) anothermodule myinst' syn_test.v
 
 echo "Test successful"
