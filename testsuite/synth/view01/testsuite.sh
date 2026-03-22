794b2c23f8e7a6e11b0cb8985321e5cf37c8a288
Tristan Gingold <tgingold@free.fr>
2026-03-22 19:03:21 +0100

    testsuite/synth: add a test in view01

--- testsuite/synth/view01/testsuite.sh
+++ testsuite/synth/view01/testsuite.sh
@@ -4,8 +4,12 @@
 
 GHDL_STD_FLAGS=--std=19
 
+
 synth axis_pkg.vhdl view01.vhdl -e > syn_view01.vhdl
 analyze axis_pkg.vhdl syn_view01.vhdl
+
+synth axis_pkg.vhdl view02.vhdl -e > syn_view02.vhdl
+analyze axis_pkg.vhdl syn_view02.vhdl
 
 clean
 
