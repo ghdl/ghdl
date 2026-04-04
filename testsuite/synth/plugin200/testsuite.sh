da5601e3891bd42271c793a04e7e80ea1921e4a9
Tristan Gingold <tgingold@free.fr>
2026-04-04 18:53:36 +0200

    testsuite/synth: adjust plugin200 baseline after previous commit

--- testsuite/synth/plugin200/testsuite.sh
+++ testsuite/synth/plugin200/testsuite.sh
@@ -8,6 +8,7 @@ if grep -F itinst_\\outp syn_test.v; then
 fi
 
 synth --out=verilog test2.vhdl -e > syn_test2.v
-grep -F "  wire \\itinst.q ;" syn_test2.v
+fgrep -q '.d(d_i)' syn_test2.v
+fgrep -q '.q(q_o)' syn_test2.v
 
 echo "Test successful"
