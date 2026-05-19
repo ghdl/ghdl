7cd72eff8134d4eb1fc4d96d5e79c411d33792e5
Tristan Gingold <tgingold@free.fr>
2026-05-19 21:10:53 +0200

    testsuite/synth: adjust #2766 after previous commit

--- testsuite/synth/issue2766/testsuite.sh
+++ testsuite/synth/issue2766/testsuite.sh
@@ -7,7 +7,7 @@ analyze --work=unisim vcomponents.vhdl ibufds.vhdl
 for f in demo_comp demo_direct; do
     analyze $f.vhdl
     synth --vendor-library=unisim $f > syn_$f.vhdl
-    grep -F ' work.ibufds ' syn_$f.vhdl
+    grep -F ': ibufds ' syn_$f.vhdl
     synth --vendor-library=unisim --out=verilog $f > syn_$f.v
     grep -F ' ibufds ' syn_$f.v
 done
